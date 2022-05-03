1bc5af002ac1fac376f5987b0b2d3d08f569b59b
Colin <colin@uninsane.org>
2022-05-03 00:08:18 +0000

    fs-config: move `/` fs mount from hardware-configuration -> fs-configuration

--- fs-configuration.nix
+++ fs-configuration.nix
@@ -1,6 +1,11 @@
 { config, pkgs, lib, ... }:
 
 {
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+    fsType = "ext4";
+  };
+
   fileSystems."/mnt/storage" = {
     device = "/dev/disk/by-uuid/2be70d38-79f4-41b6-bee2-bce5a25f8f7b";
     fsType = "ext4";
--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -13,10 +13,11 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-      fsType = "ext4";
-    };
+  # XXX colin: moved to fs-configuration.nix
+  # fileSystems."/" =
+  #   { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+  #     fsType = "ext4";
+  #   };
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
 }
