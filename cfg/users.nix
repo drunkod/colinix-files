5ca049dcbeb144bb4c1e819c695c5dc256022b18
Colin <colin@uninsane.org>
2022-05-17 02:11:52 +0000

    cfg/users: document how to create ssh keys

--- cfg/users.nix
+++ cfg/users.nix
@@ -17,6 +17,8 @@
     extraGroups = [ "wheel" ];
     initialHashedPassword = "";
     shell = pkgs.bashInteractive;
+    # XXX colin: create ssh key for THIS user by logging in and running:
+    #   ssh-keygen -t ed25519
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGSDe/y0e9PSeUwYlMPjzhW0UhNsGAGsW3lCG3apxrD5 colin@colin.desktop"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG+MZ/l5d8g5hbxMB9ed1uyvhV85jwNrSVNVxb5ujQjw colin@colin.laptop"
