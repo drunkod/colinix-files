aa979704c10bad15df9b50e2bf3b0131afb62e96
colin <colin@uninsane.org>
2022-05-22 17:17:23 -0700

    machines: pda: working image generation!
    this builds the minimal pine64-pinephone .img,
    which is about 1 GB and has no gui beyond the
    login prompt, which is inaccessible becuse usb drivers
    (for a keyboard) don't seem to be configured >_<

--- flake.nix
+++ flake.nix
@@ -68,17 +68,46 @@
         ];
       };
       img = (pkgs-mobile.lib.nixosSystem {
+        # inherit (self.genpkgs.aarch64-linux) pkgs;
         system = "aarch64-linux";
         modules = [
           mobile-nixos.nixosModules.pine64-pinephone ({
             users.users.root.password = "147147";
           })
           ({ pkgs, ... }: {
+            # This value determines the NixOS release from which the default
+            # settings for stateful data, like file locations and database versions
+            # on your system were taken. It‘s perfectly fine and recommended to leave
+            # this value at the release version of the first install of this system.
+            # Before changing this value read the documentation for this option
+            # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
             system.stateVersion = "21.11"; # Did you read the comment?
           })
-          ./image.nix
+          # ({ pkgs, mobile-nixos, ... }: {
+          #   imports = [
+          #     (import "${mobile-nixos}/lib/configuration.nix" { device = "pine64-pinephone"; })
+          #   ];
+          # })
+          # ({ pkgs, ... }: {
+          #   imports = [
+          #     <mobnixos>/devices/pine64-pinephone
+          #   ];
+          # })
         ];
-      }).config.system.build.raw;
+      }).config.mobile.outputs.u-boot.disk-image;
+      # img = nixosConfiguration.config.system.build.disk-image;
+      # img = (pkgs-mobile.lib.nixosSystem {
+      #   system = "aarch64-linux";
+      #   modules = [
+      #     mobile-nixos.nixosModules.pine64-pinephone ({
+      #       users.users.root.password = "147147";
+      #     })
+      #     ({ pkgs, ... }: {
+      #       system.stateVersion = "21.11"; # Did you read the comment?
+      #     })
+      #     ./image.nix
+      #   ];
+      # }).config.system.build.raw;
     };
 
     nixosConfigurations = builtins.mapAttrs (name: value: value.nixosConfiguration) self.machines;
