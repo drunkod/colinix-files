c880fd97d4f42a463ddbd6fce860ca15691d4d0e
colin <colin@uninsane.org>
2022-05-28 13:35:50 -0700

    add missing pubkey to nix-cache

--- helpers/universal/nix-cache.nix
+++ helpers/universal/nix-cache.nix
@@ -9,6 +9,7 @@
       "https://cache.nixos.org/"
     ];
     trusted-public-keys = [
+      "nixcache.uninsane.org:r3WILM6+QrkmsLgqVQcEdibFD7Q/4gyzD9dGT33GP70="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
