4d970f5bf5d3d375372fac6293a6614cce1c7463
colin <colin@uninsane.org>
2022-05-31 22:41:41 -0700

    remove nixpkgs-unstable from inputs
    we don't need it. the one package it was being used for is identical
    in nixpkgs stable

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.05";
-    pkgs-unstable.url = "nixpkgs/nixos-unstable";
     # pkgs-telegram.url = "nixpkgs/33775ec9a2173a08e46edf9f46c9febadbf743e8";# 2022/04/18; telegram 3.7.3. fails: nix log /nix/store/y5kv47hnv55qknb6cnmpcyraicay79fx-telegram-desktop-3.7.3.drv: g++: fatal error: cannot execute '/nix/store/njk5sbd21305bhr7gwibxbbvgbx5lxvn-gcc-9.3.0/libexec/gcc/aarch64-unknown-linux-gnu/9.3.0/cc1plus': execv: No such file or directory
     pkgs-mobile.url = "nixpkgs/dfd82985c273aac6eced03625f454b334daae2e8";    # WORKS: 2022/05/20; mobile-nixos follows this same commit.
     mobile-nixos = {
@@ -21,7 +20,7 @@
     nurpkgs.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, nixpkgs, pkgs-unstable, pkgs-mobile, mobile-nixos, home-manager, nurpkgs }: {
+  outputs = { self, nixpkgs, pkgs-mobile, mobile-nixos, home-manager, nurpkgs }: {
     machines.uninsane = self.decl-bootable-machine { name = "uninsane"; system = "aarch64-linux"; };
     machines.desko = self.decl-bootable-machine { name = "desko"; system = "x86_64-linux"; };
     machines.lappy = self.decl-bootable-machine { name = "lappy"; system = "x86_64-linux"; };
@@ -89,9 +88,6 @@
 
           # patch rpi uboot with something that fixes USB HDD boot
           ubootRaspberryPi4_64bit = next.callPackage ./pkgs/ubootRaspberryPi4_64bit { pkgs = prev; };
-
-          # we care about keeping these packages up-to-date
-          electrum = pkgs-unstable.legacyPackages.${system}.electrum;
 
           #### TEMPORARY NIXOS-UNSTABLE PACKAGES
 
