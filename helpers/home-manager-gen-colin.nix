280c568c480ace54ae5189ac698e8248e97cad30
colin <colin@uninsane.org>
2022-05-31 17:45:59 -0700

    add partial wayland support for Qt apps on phosh
    this works when they're launched from a terminal.
    but not when launched from the DE.
    probably need to apply this to global env vars.

--- helpers/home-manager-gen-colin.nix
+++ helpers/home-manager-gen-colin.nix
@@ -179,7 +179,13 @@
 
   home.sessionVariables = {
     EDITOR = "vim";
-  };
+  }
+  // (if gui != null then {
+    # all guis we support run wayland
+    # particularly on phosh, QT apps won't always start unless this env var is set
+    QT_QPA_PLATFORM = "wayland";
+  } else {});
+
 
   home.packages = [
     pkgs.btrfs-progs
