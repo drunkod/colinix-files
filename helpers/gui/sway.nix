993efdb4f4a5d7076ce747016e53f6c912831f93
colin <colin@uninsane.org>
2022-06-03 15:56:32 -0700

    sway: remove dead code

--- helpers/gui/sway.nix
+++ helpers/gui/sway.nix
@@ -20,24 +20,6 @@
   };
 
   # unlike other DEs, sway configures no audio stack
-
-  # sound (ALSA): https://nixos.wiki/wiki/ALSA
-  # test with `alsamixer`, `aplay -t raw /dev/urandom`
-  # sound = {
-  #   enable = true;
-  #   mediaKeys.enable = true;
-  # };
-  # hardware.pulseaudio.enable = true;
-  # hardware.pulseaudio.support32Bit = true;    ## If compatibility with 32-bit applications is desired.
-
-  # # disable PC-speaker
-  # boot.blacklistedKernelModules = [ "snd_pcsp" ];
-  # # enable the right driver (`cat /proc/asound/modules`)
-  # boot.extraModprobeConfig = ''
-  #   options snd slots=snd-hda-intel
-  #   options snd_hda_intel enable=0,1
-  # '';
-
   # administer with pw-cli, pw-mon, pw-top commands
   services.pipewire = {
     enable = true;
