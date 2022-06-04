0b6992e9001ee4deb136bab917b54d869a07841e
colin <colin@uninsane.org>
2022-06-04 04:10:36 -0700

    phosh: enable xwayland

--- helpers/gui/phosh.nix
+++ helpers/gui/phosh.nix
@@ -1,10 +1,12 @@
 { ... }:
 {
+  # docs: https://github.com/NixOS/nixpkgs/blob/nixos-22.05/nixos/modules/services/x11/desktop-managers/phosh.nix
   services.xserver.desktopManager.phosh = {
     enable = true;
     user = "colin";
     group = "users";
     phocConfig = {
+      xwayland = "true";
       # find default outputs by catting /etc/phosh/phoc.ini
       outputs.DSI-1 = {
         scale = 1.5;
