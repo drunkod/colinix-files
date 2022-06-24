37a7f19ecb4d71c7eae149078d15fe84622fa6be
colin <colin@uninsane.org>
2022-06-24 01:35:52 -0700

    fix bootpart-uefi-x86_64 to allow selecting old nixos generations at boot

--- pkgs/bootpart-uefi-x86_64/default.nix
+++ pkgs/bootpart-uefi-x86_64/default.nix
@@ -10,11 +10,18 @@ stdenv.mkDerivation rec {
 
   installPhase = with pkgs; ''
     # populate the EFI directory with syslinux, and configure it to read that extlinux.conf file managed by nixos
-    mkdir -p "$out/EFI/syslinux"  "$out/EFI/BOOT"
+    mkdir -p "$out/EFI/syslinux"  "$out/EFI/BOOT"  "$out/syslinux"
     cp -R "${syslinux}/share/syslinux/efi64"/* "$out/EFI/syslinux"
     echo "DEFAULT trampoline" > "$out/EFI/syslinux/syslinux.cfg"
     echo "LABEL trampoline" >> "$out/EFI/syslinux/syslinux.cfg"
-    echo "CONFIG ../../extlinux/extlinux.conf ../../extlinux" >> "$out/EFI/syslinux/syslinux.cfg"
+    echo "  SAY trampoline into generic extlinux.conf" >> "$out/EFI/syslinux/syslinux.cfg"
+    echo "  CONFIG ../../syslinux/syslinux.cfg ../../syslinux" >> "$out/EFI/syslinux/syslinux.cfg"
+
+    # we create this "trampoline" layer so that we can setup the UI directive
+    # and enable a menu before loading the real, nixos-managed extlinux.conf
+    cp "${syslinux}/share/syslinux/efi64/menu.c32" "$out/syslinux/menu.c32"
+    echo "UI menu.c32" > "$out/syslinux/syslinux.cfg"
+    echo "INCLUDE ../extlinux/extlinux.conf" >> "$out/syslinux/syslinux.cfg"
     
     # create the EFI/BOOT/BOOTX64.EFI default entry
     cp "$out/EFI/syslinux"/* "$out/EFI/BOOT"
