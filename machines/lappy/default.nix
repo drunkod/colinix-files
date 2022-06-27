d0ff605f192ed67e05b7daebe96eec9f153cad1a
colin <colin@uninsane.org>
2022-06-27 01:28:17 -0700

    lappy: setup snapper (automated btrfs snapshots)

--- machines/lappy/default.nix
+++ machines/lappy/default.nix
@@ -10,6 +10,16 @@
   boot.loader.efi.canTouchEfiVariables = false;
   colinsane.image.extraBootFiles = [ pkgs.bootpart-uefi-x86_64 ];
 
+  # default config: https://man.archlinux.org/man/snapper-configs.5
+  # defaults to something like:
+  #   - hourly snapshots
+  #   - auto cleanup; keep the last 10 hourlies, last 10 daylies, last 10 monthlys.
+  services.snapper.configs.nix = {
+    # TODO: for the impermanent setup, we'd prefer to just do /nix/persist,
+    # but that also requires setting up the persist dir as a subvol
+    subvolume = "/nix";
+  };
+
   # TODO: only here for debugging
   # services.ipfs.enable = true;
 
