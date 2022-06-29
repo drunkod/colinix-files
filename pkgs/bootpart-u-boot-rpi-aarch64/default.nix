0aa03344653b08f675c3d032035e37e7de1349af
colin <colin@uninsane.org>
2022-06-28 21:10:50 -0700

    new bootpart for u-boot rpi
    this needs some tweaking: it doesn't package the right dtb file.
    it packages the dtb for the linux kernels, whereas we want dtbs to be
    consumed by u-boot.

---
+++ pkgs/bootpart-u-boot-rpi-aarch64/default.nix
@@ -0,0 +1,37 @@
+{ stdenv, pkgs }:
+
+stdenv.mkDerivation rec {
+  pname = "bootpart-tow-boot-rpi-aarch64";
+  version = "1";
+
+  buildInputs = with pkgs; [ 
+    ubootRaspberryPi4_64bit
+    raspberrypifw  # for bootcode.bin, *.dat, *.elf, *.dtb
+    raspberrypi-armstubs  # for armstub*
+
+  ];
+
+  src = ./config.txt;
+
+  dontUnpack = true;
+
+  installPhase = with pkgs; ''
+    mkdir "$out"
+    cp ${ubootRaspberryPi4_64bit}/u-boot.bin "$out"/
+    cp -R ${raspberrypifw}/share/raspberrypi/boot/*.dtb "$out"/
+    cp -R ${raspberrypifw}/share/raspberrypi/boot/*.bin "$out"/
+    cp -R ${raspberrypifw}/share/raspberrypi/boot/*.dat "$out"/
+    cp -R ${raspberrypifw}/share/raspberrypi/boot/*.img "$out"/
+    cp -R ${raspberrypifw}/share/raspberrypi/boot/*.elf "$out"/
+    cp -R ${raspberrypifw}/share/raspberrypi/boot/overlays "$out"/
+    cp ${raspberrypi-armstubs}/* "$out"/
+    cp ${src} "$out"/config.txt
+  '';
+
+
+  meta = {
+    description = "unmanaged files to place in /boot on a raspberry pi system";
+    platforms = [ "aarch64-linux" ];
+  };
+}
+
