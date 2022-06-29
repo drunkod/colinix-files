1f3c93623f14d5232e4e2519d7962ab4158c48f6
colin <colin@uninsane.org>
2022-06-29 03:58:11 -0700

    desko: enable snapper for testing

--- machines/desko/default.nix
+++ machines/desko/default.nix
@@ -18,6 +18,20 @@
   # needed to use libimobiledevice/ifuse, for iphone sync
   services.usbmuxd.enable = true;
 
+  # default config: https://man.archlinux.org/man/snapper-configs.5
+  # defaults to something like:
+  #   - hourly snapshots
+  #   - auto cleanup; keep the last 10 hourlies, last 10 daylies, last 10 monthlys.
+  services.snapper.configs.nix = {
+    # TODO: for the impermanent setup, we'd prefer to just do /nix/persist,
+    # but that also requires setting up the persist dir as a subvol
+    subvolume = "/nix";
+    # TODO: ALLOW_USERS doesn't seem to work. still need `sudo snapper -c nix list`
+    extraConfig = ''
+      ALLOW_USERS = "colin";
+    '';
+  };
+
   sops.secrets.duplicity_passphrase = {
     sopsFile = ../../secrets/desko.yaml;
   };
