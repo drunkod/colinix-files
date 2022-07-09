f790147fb013fa9b52a20f18ee5012a637694d6a
colin <colin@uninsane.org>
2022-07-09 00:48:09 -0700

    add ukraine VPN

--- modules/universal/vpn.nix
+++ modules/universal/vpn.nix
@@ -25,7 +25,34 @@
     autostart = false;
   };
 
+  networking.wg-quick.interfaces.ovpnd-ukr = {
+    address = [
+      "172.18.180.159/32"
+      "fd00:0000:1337:cafe:1111:1111:ec5c:add3/128"
+    ];
+    dns = [
+      "46.227.67.134"
+      "192.165.9.158"
+    ];
+    peers = [
+      {
+        allowedIPs = [
+          "0.0.0.0/0"
+          "::/0"
+        ];
+        endpoint = "vpn96.prd.kyiv.ovpn.com:9929";
+        publicKey = "CjZcXDxaaKpW8b5As1EcNbI6+42A6BjWahwXDCwfVFg=";
+      }
+    ];
+    privateKeyFile = config.sops.secrets.wg_ovpnd_ukr_privkey.path;
+    # to start: `systemctl start wg-quick-ovpnd-ukr`
+    autostart = false;
+  };
+
   sops.secrets."wg_ovpnd_privkey" = {
+    sopsFile = ../../secrets/universal.yaml;
+  };
+  sops.secrets."wg_ovpnd_ukr_privkey" = {
     sopsFile = ../../secrets/universal.yaml;
   };
 }
