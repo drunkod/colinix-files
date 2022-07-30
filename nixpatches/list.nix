641b32b8d0a49315184699035ef76e14da037004
colin <colin@uninsane.org>
2022-07-29 20:08:35 -0700

    fix jackett build (by disabling the check phase. manually verified: still works)

--- nixpatches/list.nix
+++ nixpatches/list.nix
@@ -9,11 +9,22 @@ fetchpatch: [
   ./02-rpi4-uboot.patch
   # alternative to https://github.com/NixOS/nixpkgs/pull/173200
   ./04-dart-2.7.0.patch
-  # whalebird: suuport aarch64
+  # whalebird: support aarch64
   (fetchpatch {
     url = "https://github.com/NixOS/nixpkgs/pull/176476.diff";
     sha256 = "sha256-126DljM06hqPZ3fjLZ3LBZR64nFbeTfzSazEu72d4y8=";
   })
+
+  # jackett updates
+  # (fetchpatch {
+  #   url = "https://github.com/NixOS/nixpkgs/pull/169395.diff";
+  #   sha256 = "sha256-JZh/5I22ZALnzaWQXAbvOJ1ZVjuRpPNY5WWFJDalzXk=";
+  # })
+  # (fetchpatch {
+  #   url = "https://github.com/NixOS/nixpkgs/pull/182847.diff";
+  #   sha256 = "sha256-EojqEVNBIyM4DJYLMiY+UKey8RV4tuIv0uyTMEhKiRo=";
+  # })
+
   # TODO: upstream
   ./07-duplicity-rich-url.patch
   # TODO: upstream
