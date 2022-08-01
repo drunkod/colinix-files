ac772e72b84a43a115f1c5e2b5057fb7fa0c8619
colin <colin@uninsane.org>
2022-07-31 18:09:28 -0700

    document how to make Electron apps use wayland natively

--- modules/universal/env/default.nix
+++ modules/universal/env/default.nix
@@ -13,6 +13,10 @@
     EDITOR = "vim";
     # git claims it should use EDITOR, but it doesn't!
     GIT_EDITOR = "vim";
+    # Electron apps should use native wayland backend:
+    #   https://nixos.wiki/wiki/Slack#Wayland
+    # Discord under sway crashes with this.
+    # NIXOS_OZONE_WL = "1";
   };
 }
 
