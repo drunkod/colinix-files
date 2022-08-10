8d2c8d44f386095100230caefd6acf390c4b0079
colin <colin@uninsane.org>
2022-08-09 19:49:09 -0700

    disable kaiteki: its packaging is non-deterministic and i can't get a stable hash for which it builds

--- modules/universal/env/home-packages.nix
+++ modules/universal/env/home-packages.nix
@@ -93,8 +93,8 @@ let
       nss = pkgs.nss_latest;
     }); in { pkg = discord; dir = ".config/discord"; })
 
-    kaiteki  # Pleroma client
-    gnome.zenity # for kaiteki (it will use qarma, kdialog, or zenity)
+    # kaiteki  # Pleroma client
+    # gnome.zenity # for kaiteki (it will use qarma, kdialog, or zenity)
 
     logseq
     losslesscut-bin
