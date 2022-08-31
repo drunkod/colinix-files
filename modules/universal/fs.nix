85add7c531c5621bb82d89660c6df81e25b79e5b
colin <colin@uninsane.org>
2022-08-30 19:55:15 -0700

    add a script to mount servo root

--- modules/universal/fs.nix
+++ modules/universal/fs.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 
-let sshOpts = {
+let sshOpts = rec {
   fsType = "fuse.sshfs";
-  options = [
+  optionsBase = [
     "x-systemd.automount"
     "_netdev"
     "user"
@@ -11,23 +11,44 @@ let sshOpts = {
     "identityfile=/home/colin/.ssh/id_ed25519"
     "allow_other"
     "default_permissions"
+  ];
+  optionsColin = optionsBase ++ [
     "uid=1000"
     "gid=100"
   ];
+  # optionsRoot = optionsBase ++ [
+  #   "uid=0"
+  #   "gid=0"
+  # ];
 };
 in
 {
   fileSystems."/mnt/servo-media-wan" = {
     device = "colin@uninsane.org:/var/lib/uninsane/media";
-    inherit (sshOpts) fsType options;
+    inherit (sshOpts) fsType;
+    options = sshOpts.optionsColin;
   };
   fileSystems."/mnt/servo-media-lan" = {
     device = "colin@servo:/var/lib/uninsane/media";
-    inherit (sshOpts) fsType options;
+    inherit (sshOpts) fsType;
+    options = sshOpts.optionsColin;
+  };
+  fileSystems."/mnt/servo-root-wan" = {
+    device = "colin@uninsane.org:/";
+    inherit (sshOpts) fsType;
+    # options = sshOpts.optionsRoot;
+    options = sshOpts.optionsColin;
+  };
+  fileSystems."/mnt/servo-root-lan" = {
+    device = "colin@servo:/";
+    inherit (sshOpts) fsType;
+    # options = sshOpts.optionsRoot;
+    options = sshOpts.optionsColin;
   };
   fileSystems."/mnt/desko-home" = {
     device = "colin@desko:/home/colin";
-    inherit (sshOpts) fsType options;
+    inherit (sshOpts) fsType;
+    options = sshOpts.optionsColin;
   };
 
   environment.systemPackages = [
