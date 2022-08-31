cb98ac2a918b54b3a45dd1421a66bebe99be07ff
colin <colin@uninsane.org>
2022-08-30 21:14:12 -0700

    fs: add desko-root

--- modules/universal/fs.nix
+++ modules/universal/fs.nix
@@ -6,20 +6,19 @@ let sshOpts = rec {
     "x-systemd.automount"
     "_netdev"
     "user"
-    "idmap=user"
     "transform_symlinks"
     "identityfile=/home/colin/.ssh/id_ed25519"
     "allow_other"
     "default_permissions"
   ];
   optionsColin = optionsBase ++ [
+    "idmap=user"
     "uid=1000"
     "gid=100"
   ];
-  # optionsRoot = optionsBase ++ [
-  #   "uid=0"
-  #   "gid=0"
-  # ];
+  optionsRoot = optionsBase ++ [
+    "sftp_server=/run/wrappers/bin/sudo\\040/nix/store/96idbd49a410sm35kfz7j8rzp5g983qb-openssh-9.0p1/libexec/sftp-server"
+  ];
 };
 in
 {
@@ -36,19 +35,22 @@ in
   fileSystems."/mnt/servo-root-wan" = {
     device = "colin@uninsane.org:/";
     inherit (sshOpts) fsType;
-    # options = sshOpts.optionsRoot;
-    options = sshOpts.optionsColin;
+    options = sshOpts.optionsRoot;
   };
   fileSystems."/mnt/servo-root-lan" = {
     device = "colin@servo:/";
     inherit (sshOpts) fsType;
-    # options = sshOpts.optionsRoot;
-    options = sshOpts.optionsColin;
+    options = sshOpts.optionsRoot;
   };
   fileSystems."/mnt/desko-home" = {
     device = "colin@desko:/home/colin";
     inherit (sshOpts) fsType;
     options = sshOpts.optionsColin;
+  };
+  fileSystems."/mnt/desko-root" = {
+    device = "colin@desko:/";
+    inherit (sshOpts) fsType;
+    options = sshOpts.optionsRoot;
   };
 
   environment.systemPackages = [
