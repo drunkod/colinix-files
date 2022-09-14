ee3793ad46b12f523e742f5968351a09fb7b8a08
colin <colin@uninsane.org>
2022-09-14 14:45:33 -0700

    nixcache: use desko as a substituter

--- modules/nixcache.nix
+++ modules/nixcache.nix
@@ -17,11 +17,13 @@ in
     nix.settings = {
       substituters = [
         "https://nixcache.uninsane.org"
+        "http://desko:5000"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org/"
       ];
       trusted-public-keys = [
         "nixcache.uninsane.org:r3WILM6+QrkmsLgqVQcEdibFD7Q/4gyzD9dGT33GP70="
+        "desko:Q7mjjqoBMgNQ5P0e63sLur65A+D4f3Sv4QiycDIKxiI="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
