dc44d8098ecb58070761176cdc6ed21f16a0e080
colin <colin@uninsane.org>
2022-09-16 04:49:15 -0700

    fix flutter build for aarch64

--- nixpatches/list.nix
+++ nixpatches/list.nix
@@ -10,29 +10,28 @@ fetchpatch: [
   #   (it's a dupe of https://github.com/NixOS/nixpkgs/pull/112677 )
   ./02-rpi4-uboot.patch
 
-  # TODO: more patches are required prior to this one
-  # flutter.dart: 2.16.1 -> 2.16.2
-  (fetchpatch {
-    url = "https://github.com/NixOS/nixpkgs/pull/172873.diff";
-    sha256 = "sha256-HGYk83XOhFe1wWNCKNdF6s/7laWJ0Jisb8bDJcHVlsQ=";
-  })
-  # Flutter: 2.10.1->3.0.4
-  (fetchpatch {
-    url = "https://github.com/NixOS/nixpkgs/pull/173200.diff";
-    sha256 = "sha256-g1tZdLTrAJx3ijgabqz8XInC20PQM3FYRENQ7c6NfQw=";
-  })
-  # dart: 2.17.3 -> 2.18.0
-  (fetchpatch {
-    url = "https://github.com/NixOS/nixpkgs/pull/189841.diff";
-    sha256 = "sha256-E3rTNex7PiFHsLgtb0x9/Q/naqDYD1vFuGyduV4Z6qY=";
-  })
+  # # flutter.dart: 2.16.1 -> 2.16.2
+  # (fetchpatch {
+  #   url = "https://github.com/NixOS/nixpkgs/pull/172873.diff";
+  #   sha256 = "sha256-HGYk83XOhFe1wWNCKNdF6s/7laWJ0Jisb8bDJcHVlsQ=";
+  # })
+  # # Flutter: 2.10.1->3.0.4
+  # (fetchpatch {
+  #   url = "https://github.com/NixOS/nixpkgs/pull/173200.diff";
+  #   sha256 = "sha256-g1tZdLTrAJx3ijgabqz8XInC20PQM3FYRENQ7c6NfQw=";
+  # })
+  # # dart: 2.17.3 -> 2.18.0
+  # (fetchpatch {
+  #   url = "https://github.com/NixOS/nixpkgs/pull/189841.diff";
+  #   sha256 = "sha256-E3rTNex7PiFHsLgtb0x9/Q/naqDYD1vFuGyduV4Z6qY=";
+  # })
+  # # # Flutter: 3.0.4->3.3.2, flutter.dart: 2.17.5->2.18.1
+  # # (fetchpatch {
+  # #   url = "https://github.com/NixOS/nixpkgs/pull/189338.diff";
+  # #   sha256 = "sha256-MppSk1D3qQT8Z4lzEZ93UexoidT8yqM7ASPec4VvxCI=";
+  # # })
   # enable aarch64 support for flutter's dart package
   ./10-flutter-arm64.patch
-  # Flutter: 3.0.4->3.3.2, flutter.dart: 2.17.5->2.18.1
-  # (fetchpatch {
-  #   url = "https://github.com/NixOS/nixpkgs/pull/189338.diff";
-  #   sha256 = "sha256-MppSk1D3qQT8Z4lzEZ93UexoidT8yqM7ASPec4VvxCI=";
-  # })
 
 
   # whalebird: support aarch64
