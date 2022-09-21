e66692eecda44373fd51d85c3832f47479bc3113
colin <colin@uninsane.org>
2022-09-21 02:58:35 -0700

    enable fluffychat -- though reproducibility may suffer
    i've applied this patch, but modified the sha:
    - <https://github.com/NixOS/nixpkgs/pull/186839>

--- nixpatches/list.nix
+++ nixpatches/list.nix
@@ -4,6 +4,12 @@ fetchpatch: [
   #   (it's a dupe of https://github.com/NixOS/nixpkgs/pull/112677 )
   ./02-rpi4-uboot.patch
 
+  # (fetchpatch {
+  #   url = "https://github.com/NixOS/nixpkgs/pull/186839.diff";
+  #   sha256 = "sha256-NdIfie+eTy4V1vgqiiRPtWdnxZ5ZHsvCMfkEDUv9SC8=";
+  # })
+  ./11-186839-fluffychat1.2.0-1.6.1.patch
+
   # # # Flutter: 3.0.4->3.3.2, flutter.dart: 2.17.5->2.18.1
   # # (fetchpatch {
   # #   url = "https://github.com/NixOS/nixpkgs/pull/189338.diff";
