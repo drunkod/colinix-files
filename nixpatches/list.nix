00c22c1ca778bf4d5848e3618cd458d63387a261
colin <colin@uninsane.org>
2022-09-21 04:29:44 -0700

    fix flutter build (x86-64; arm is untested)

--- nixpatches/list.nix
+++ nixpatches/list.nix
@@ -4,11 +4,10 @@ fetchpatch: [
   #   (it's a dupe of https://github.com/NixOS/nixpkgs/pull/112677 )
   ./02-rpi4-uboot.patch
 
-  # (fetchpatch {
-  #   url = "https://github.com/NixOS/nixpkgs/pull/186839.diff";
-  #   sha256 = "sha256-NdIfie+eTy4V1vgqiiRPtWdnxZ5ZHsvCMfkEDUv9SC8=";
-  # })
-  ./11-186839-fluffychat1.2.0-1.6.1.patch
+  (fetchpatch {
+    url = "https://github.com/NixOS/nixpkgs/pull/186839.diff";
+    sha256 = "sha256-NdIfie+eTy4V1vgqiiRPtWdnxZ5ZHsvCMfkEDUv9SC8=";
+  })
 
   # # # Flutter: 3.0.4->3.3.2, flutter.dart: 2.17.5->2.18.1
   # # (fetchpatch {
@@ -16,7 +15,7 @@ fetchpatch: [
   # #   sha256 = "sha256-MppSk1D3qQT8Z4lzEZ93UexoidT8yqM7ASPec4VvxCI=";
   # # })
   # enable aarch64 support for flutter's dart package
-  # ./10-flutter-arm64.patch
+  ./10-flutter-arm64.patch
 
 
   # TODO: upstream
