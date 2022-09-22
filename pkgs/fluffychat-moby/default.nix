4b3b71bb847494ab243e871028c1ade0a0372c8c
colin <colin@uninsane.org>
2022-09-22 05:25:18 -0700

    fluffychat: mention TODO for desktop files

--- pkgs/fluffychat-moby/default.nix
+++ pkgs/fluffychat-moby/default.nix
@@ -8,6 +8,9 @@
   # > Failed to start Flutter renderer: Unable to create a GL context
   # this is temporarily solved by using software renderer
   # - see https://github.com/flutter/flutter/issues/106941
+  #
+  # TODO: the desktop files reference the uwrapped fluffychat and need to be updated.
+  # as is this only works when fluffychat is launched from the CLI
   postBuild = ''
     wrapProgram $out/bin/fluffychat \
       --set LIBGL_ALWAYS_SOFTWARE 1
