b3a4a95e283685eb7194deddf4092f1487e98288
colin <colin@uninsane.org>
2022-09-23 15:29:33 -0700

    servo: disable jellyfin to stop syslog spam
    i also don't use it much anymore. mostly navidrome.
    may be useful in future when i want DLNA support.

--- machines/servo/services/jellyfin.nix
+++ machines/servo/services/jellyfin.nix
@@ -5,7 +5,10 @@
     # TODO: mode? could be more granular
     { user = "jellyfin"; group = "jellyfin"; directory = "/var/lib/jellyfin"; }
   ];
-  users.users.jellyfin.uid = config.sane.allocations.jellyfin-uid;
-  users.groups.jellyfin.gid = config.sane.allocations.jellyfin-gid;
-  services.jellyfin.enable = true;
+
+  # users.users.jellyfin.uid = config.sane.allocations.jellyfin-uid;
+  # users.groups.jellyfin.gid = config.sane.allocations.jellyfin-gid;
+  # TODO: re-enable after migrating media dir to /var/lib/uninsane/media
+  # else it's too spammy
+  # services.jellyfin.enable = true;
 }
