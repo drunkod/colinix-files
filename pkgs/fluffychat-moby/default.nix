09e8510d0e2df86c66788a0450bdf5c0d4c75a86
colin <colin@uninsane.org>
2022-09-22 17:44:42 -0700

    fluffychat: fix .desktop file to reference the wrapped version

--- pkgs/fluffychat-moby/default.nix
+++ pkgs/fluffychat-moby/default.nix
@@ -4,15 +4,17 @@
   name = "fluffychat-moby";
   paths = [ pkgs.fluffychat ];
   buildInputs = [ pkgs.makeWrapper ];
+
   # ordinary fluffychat on moby displays blank window;
   # > Failed to start Flutter renderer: Unable to create a GL context
   # this is temporarily solved by using software renderer
   # - see https://github.com/flutter/flutter/issues/106941
-  #
-  # TODO: the desktop files reference the uwrapped fluffychat and need to be updated.
-  # as is this only works when fluffychat is launched from the CLI
   postBuild = ''
     wrapProgram $out/bin/fluffychat \
       --set LIBGL_ALWAYS_SOFTWARE 1
+    # fix up the .desktop file to invoke our wrapped fluffychat
+    orig_desktop=$(readlink $out/share/applications/Fluffychat.desktop)
+    unlink $out/share/applications/Fluffychat.desktop
+    sed "s:Exec=.*:Exec=$out/bin/fluffychat:" $orig_desktop > $out/share/applications/Fluffychat.desktop
   '';
 })
