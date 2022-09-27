d466c0b942b515db2ef7ca4512c1f0cfbe8a0c03
colin <colin@uninsane.org>
2022-09-26 23:20:31 -0700

    bump kaiteki: 2022-06-03 -> unstable-2022-08-31

--- pkgs/kaiteki/default.nix
+++ pkgs/kaiteki/default.nix
@@ -2,25 +2,32 @@
 , fetchFromGitHub
 , flutter
 , makeDesktopItem
+, xdg-user-dirs
 }:
 
 flutter.mkFlutterApp rec {
   pname = "kaiteki";
-  version = "unstable-2022-06-03";
+  version = "unstable-2022-08-31";
 
   # this hash seems unstable -- depends on other nixpkgs, perhaps?
-  vendorHash = "sha256-IC3FAPFASuMcNOpUuaB+MDcm9nqGCtq/6A2dCxIXHEg=";
+  # vendorHash = "sha256-4DdtceIxj89ta12EhfxA2B5K9++LrKSA7ZzUb/NaaGs=";
+  vendorHash = "sha256-OEDJSnXDNut/YZ+3cQ76KUBW/MaXcnirZDHu/n97108=";
 
   src = fetchFromGitHub {
     owner = "Kaiteki-Fedi";
     repo = "Kaiteki";
-    rev = "0a322313071e4391949d23d9b006d74de65f58d9";
-    hash = "sha256-ggDIbVwueS162m15TFaC6Tcg+0lpcVGi4x/O691sxR8";
+    # rev = "cf94ec55063cd7af20a37103fc40c588a634962f";
+    # hash = "sha256-jtRT0Q4/i3dxRYcC6HPClL9Iw1PizkIUgswU1eusKig=";
+    # past this hash Kaiteki introduces submodules for l10n stuff; not sure how to account for that yet.
+    rev = "324077e4716ce996531457ec9c45fb3cc82820a0";
+    hash = "sha256-qqVePcDnuc7SdPUhZtfcMGPzpemmZEvCNLqEbUDi2SA=";
   };
+
+  nativeBuildInputs = [ xdg-user-dirs ];
 
   desktopItems = [ (makeDesktopItem {
     name = "Kaiteki";
-    exec = "kaiteki";
+    exec = "@out@/bin/kaiteki";
     icon = "kaiteki";
     desktopName = "Kaiteki";
     genericName = "Micro-blogging client";
@@ -30,11 +37,16 @@ flutter.mkFlutterApp rec {
 
   sourceRoot = "source/src/kaiteki";
 
+  postInstall = ''
+    wrapProgram $out/bin/kaiteki \
+      --prefix PATH : "${xdg-user-dirs}/bin"
+  '';
+
   meta = with lib; {
     description = "The comfy Fediverse client";
     homepage = "https://craftplacer.moe/projects/kaiteki/";
     license = licenses.agpl3Plus;
-    # maintainers = with maintainers; [ colinsane ];
+    maintainers = with maintainers; [ colinsane ];
     platforms = platforms.linux;
   };
 }
