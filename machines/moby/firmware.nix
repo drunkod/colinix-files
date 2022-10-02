7d4a7df2ddb8907da204708d75a2b1347015e07a
colin <colin@uninsane.org>
2022-10-01 23:51:29 -0700

    replace deprecated `runCommandNoCC` with `runCommand` (fixes imgs.moby build)

--- machines/moby/firmware.nix
+++ machines/moby/firmware.nix
@@ -4,7 +4,7 @@
   # only actually need 1 MB, but better to over-allocate than under-allocate
   sane.image.extraGPTPadding = 16 * 1024 * 1024;
   sane.image.firstPartGap = 0;
-  system.build.img = pkgs.runCommandNoCC "nixos_full-disk-image.img" {} ''
+  system.build.img = pkgs.runCommand "nixos_full-disk-image.img" {} ''
     cp -v ${config.system.build.img-without-firmware}/nixos.img $out
     chmod +w $out
     dd if=${pkgs.tow-boot-pinephone}/Tow-Boot.noenv.bin of=$out bs=1024 seek=8 conv=notrunc
