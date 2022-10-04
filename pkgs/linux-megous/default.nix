064882576542784074b2543df83eb7b46c9edda6
colin <colin@uninsane.org>
2022-10-04 15:57:15 -0700

    moby: update kernel 6.0.0-rc4 -> 6.0.0 (release)

--- pkgs/linux-megous/default.nix
+++ pkgs/linux-megous/default.nix
@@ -3,10 +3,10 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.0.0-rc4";
+  version = "6.0.0";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
-  modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) + "-rc4" else modDirVersionArg;
+  modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
 
   # branchVersion needs to be x.y
   extraMeta.branch = versions.majorMinor version;
@@ -15,7 +15,7 @@ buildLinux (args // rec {
     owner = "megous";
     repo = "linux";
     # branch: orange-pi-6.0
-    rev = "6ada3caab0b37968f1257b3ea75e5b0466a77162";
-    sha256 = "sha256-jIhOE0ZMuoJm7NqAEJ4OTNLHN/h8i4cOphcw3le7RSw=";
+    rev = "b16232c6156de17e1dfdb63fdaea8e317baa07a7";
+    sha256 = "sha256-Tb05IQKFdX/T7elGNnXTLVmgGLvXoeBFBq/8Q7jQhX0=";
   };
 } // (args.argsOverride or { }))
