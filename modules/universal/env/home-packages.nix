f64c44716e343868b948413d0389972ec5223c5a
colin <colin@uninsane.org>
2022-10-08 05:41:32 -0700

    home: persist fractal IM data

--- modules/universal/env/home-packages.nix
+++ modules/universal/env/home-packages.nix
@@ -62,7 +62,12 @@ let
 
     foliate
     font-manager
-    fractal-next
+
+    # XXX by default fractal stores its state in ~/.local/share/<UUID>.
+    # after logging in, manually change ~/.local/share/keyrings/... to point it to some predictable subdir.
+    # then reboot (so that libsecret daemon re-loads the keyring...?)
+    { pkg = fractal-next; dir = ".local/share/fractal"; }
+
     gimp  # broken on phosh
     gnome.cheese
     gnome.dconf-editor
