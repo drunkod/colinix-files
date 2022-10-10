85e249913a2f2f7e3cd8c71489b299c63cd17648
colin <colin@uninsane.org>
2022-10-09 17:01:48 -0700

    update: nixpkgs 2022-10-06 -> 2022-10-08

--- nixpatches/list.nix
+++ nixpatches/list.nix
@@ -1,17 +1,4 @@
 fetchpatch: [
-  # Flutter: 3.0.4 -> 3.3.3, flutter.dart: 2.17.5 -> 2.18.2
-  # merged 2022/10/07
-  # (fetchpatch {
-  #   url = "https://github.com/NixOS/nixpkgs/pull/189338.diff";
-  #   sha256 = "sha256-HRkOIBcOnSXyTKkYxnMgZou8MHU/5eNhxxARdUq9UWg=";
-  #   # url = "https://git.uninsane.org/colin/nixpkgs/commit/889c3a8cbc91c0d10b34ab7825fa1f6d1d31668a.diff";
-  #   # sha256 = "sha256-qVWLpNoW3HVSWRtXS1BcSusKOq0CAMfY0BVU9MxPm98=";
-  # })
-  #
-  # XXX this is a cherry-pick of all the commits in PR 189338 (as appears in tree).
-  # the diff yielded by Github is apparently not the same somehow (maybe because the branches being merged had diverged too much?)
-  ./11-flutter-3.3.3-189338.patch
-
   # phosh-mobile-settings: init at 0.21.1
   (fetchpatch {
     url = "http://git.uninsane.org/colin/nixpkgs/commit/0c1a7e8504291eb0076bbee3f8ebf693f4641112.diff";
