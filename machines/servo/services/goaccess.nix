8dd267db30b69eb9c0e6751e15358ae462bfe429
colin <colin@uninsane.org>
2022-10-24 00:16:42 -0700

    servo: goaccess: anonymize IPs and hide the 'HOSTS' panel

--- machines/servo/services/goaccess.nix
+++ machines/servo/services/goaccess.nix
@@ -3,6 +3,8 @@
   # based on <https://bytes.fyi/real-time-goaccess-reports-with-nginx/>
   # log-format setting can be derived with this tool if custom:
   # - <https://github.com/stockrt/nginx2goaccess>
+  # config options:
+  # - <https://github.com/allinurl/goaccess/blob/master/config/goaccess.conf>
 
   systemd.services.goaccess = {
     description = "GoAccess server monitoring";
@@ -12,6 +14,9 @@
           -f /var/log/nginx/public.log \
           --log-format=VCOMBINED \
           --real-time-html \
+          --no-query-string \
+          --anonymize-ip \
+          --ignore-panel=HOSTS \
           --ws-url=wss://sink.uninsane.org:443/ws \
           --port=7890 \
           -o /var/lib/uninsane/sink/index.html
