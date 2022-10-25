c44cad9c16c907e4beab179075bdf2e732fde0bf
colin <colin@uninsane.org>
2022-10-25 02:12:55 -0700

    fractal: persist data in ~/private

--- modules/universal/home-packages.nix
+++ modules/universal/home-packages.nix
@@ -66,7 +66,7 @@ let
     # XXX by default fractal stores its state in ~/.local/share/<UUID>.
     # after logging in, manually change ~/.local/share/keyrings/... to point it to some predictable subdir.
     # then reboot (so that libsecret daemon re-loads the keyring...?)
-    { pkg = fractal-next; dir = ".local/share/fractal"; }
+    { pkg = fractal-next; private = ".local/share/fractal"; }
 
     gimp  # broken on phosh
     gnome.cheese
