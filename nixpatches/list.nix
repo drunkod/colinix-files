4b5accac88501a41f2922bbcf41d1e93f5bc6de1
colin <colin@uninsane.org>
2022-10-30 21:35:39 -0700

    flake update: `nixpkgs: 2022-10-22 -> 2022-10-29` and others
    ```
    • Updated input 'mobile-nixos':
        'github:nixos/mobile-nixos/1351091d2537040454fa232d8b94e745ab0eb5a3' (2022-10-24)
      → 'github:nixos/mobile-nixos/da56c338a2b00c868697b75bdbd388f60d50c820' (2022-10-30)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/95aeaf83c247b8f5aa561684317ecd860476fcd6' (2022-10-22)
      → 'github:NixOS/nixpkgs/fdebb81f45a1ba2c4afca5fd9f526e1653ad0949' (2022-10-29)
    • Updated input 'nixpkgs-stable':
        'github:NixOS/nixpkgs/3933d8bb9120573c0d8d49dc5e890cb211681490' (2022-10-22)
      → 'github:NixOS/nixpkgs/26eb67abc9a7370a51fcb86ece18eaf19ae9207f' (2022-10-30)
    • Updated input 'rycee':
        'gitlab:rycee/nur-expressions/43d3a363c126968db46585b88b8eb97dd32634ad' (2022-10-27)
      → 'gitlab:rycee/nur-expressions/5fb3c4733c00a7e7be69877d057f6760d85cecb8' (2022-10-29)
    • Updated input 'sops-nix':
        'github:Mic92/sops-nix/1b5f9512a265f0c9687dbff47893180f777f4809' (2022-10-23)
      → 'github:Mic92/sops-nix/448ec3e7eb7c7e4563cc2471db748a71baaf9698' (2022-10-30)
    • Updated input 'sops-nix/nixpkgs-22_05':
        'github:NixOS/nixpkgs/f9115594149ebcb409a42e303bec4956814a8419' (2022-10-23)
      → 'github:NixOS/nixpkgs/6440d13df2327d2db13d3b17e419784020b71d22' (2022-10-30)
    • Updated input 'uninsane':
        'git+https://git.uninsane.org/colin/uninsane?ref=refs%2fheads%2fmaster&rev=4ad1801f6cecd678bbeae5dfe5933448dd7b3360' (2022-10-14)
      → 'git+https://git.uninsane.org/colin/uninsane?ref=refs%2fheads%2fmaster&rev=80c6ec95bd430e29d231cf745f19279bb76fb382' (2022-10-27)
    ```

--- nixpatches/list.nix
+++ nixpatches/list.nix
@@ -6,13 +6,6 @@ fetchpatch: [
     sha256 = "sha256-OczjlQcG7sTM/V9Y9VL/qdwaWPKfjAJsh3czqqhRQig=";
   })
 
-  # freshrss: fix ExecStart path
-  (fetchpatch {
-    url = "https://github.com/NixOS/nixpkgs/pull/197731.diff";
-    # url = "http://git.uninsane.org/colin/nixpkgs/commit/e4235c60b71bec66fe8f811cdbdd229bcf98915f.diff";
-    sha256 = "sha256-SL7tddw0YZWzZ+JhosoTyBuEahEJEjMuV4WEBCg9OM0=";
-  })
-
 
   # # kaiteki: init at 2022-09-03
   # vendorHash changes too frequently (might not be reproducible).
@@ -23,17 +16,6 @@ fetchpatch: [
   #   sha256 = "sha256-UWnfS+stVpUZ3Sfaym9XtVBlwvHWJVMaW7cYIcf3M5Q=";
   # })
 
-  # nautilus: look for the gtk4 FileChooser settings instead of the gtk4 one
-  (fetchpatch {
-    # original version (include the patch in nixpkgs)
-    # url = "https://git.uninsane.org/colin/nixpkgs/commit/4636a04c1c4982a0e71ae77d3aa6f52d1a3170f1.diff";
-    # sha256 = "sha256-XKfXStdcveYuk58rlORVJOv0a9Q5aRj1bYT5k79rL0g=";
-
-    # v2 (fetchpatch from upstream PR)
-    # url = "https://git.uninsane.org/colin/nixpkgs/commit/730a802808c549220144e4e62aa419bb07c5ae29.diff";
-    url = "https://github.com/NixOS/nixpkgs/pull/195985.diff";
-    sha256 = "sha256-PJsjFEqqtNZ5QqXx6pVFAactEIJ7r658KL7g7cDR148=";
-  })
 
   # Fix mk flutter app
   # closed (not merged). updates fluffychat 1.2.0 -> 1.6.1, but unstable hashing
@@ -48,6 +30,7 @@ fetchpatch: [
   ./02-rpi4-uboot.patch
 
   # TODO: upstream
+  # maybe convert this patch to add a `targetUrlExpr` instead of doing the `escapeShellArgs` hack
   ./07-duplicity-rich-url.patch
 
   # enable aarch64 support for flutter's dart package
