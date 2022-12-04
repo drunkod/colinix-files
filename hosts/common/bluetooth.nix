d5d89a10b92f25f19f0474ef1e0bde6d0e6ca2ca
colin <colin@uninsane.org>
2022-12-04 10:56:50 +0000

    bluetooth: add key for connecting to my car

--- hosts/common/bluetooth.nix
+++ hosts/common/bluetooth.nix
@@ -17,6 +17,10 @@
   );
 
   # TODO: use a glob, or a list, or something?
+  sops.secrets."bt/car" = {
+    sopsFile = ../../secrets/universal/bt/car.bin;
+    format = "binary";
+  };
   sops.secrets."bt/portable-speaker" = {
     sopsFile = ../../secrets/universal/bt/portable-speaker.bin;
     format = "binary";
