a8a4b8e739aa1c1ee7379658c7475d0263c2ac70
colin <colin@uninsane.org>
2022-12-16 05:58:51 +0000

    kiwix: serve the full english Wikipedia

--- hosts/servo/services/kiwix-serve.nix
+++ hosts/servo/services/kiwix-serve.nix
@@ -3,6 +3,6 @@
   sane.services.kiwix-serve = {
     enable = true;
     port = 8013;
-    zimPaths = [ "/var/lib/uninsane/www-archive/wikipedia_en_simple_all_mini_2022-11.zim" ];
+    zimPaths = [ "/var/lib/uninsane/www-archive/wikipedia_en_all_maxi_2022-05.zim" ];
   };
 }
