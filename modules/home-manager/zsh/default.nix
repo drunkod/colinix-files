69361ee9a25e86810a42472b0617b24ac93dc644
colin <colin@uninsane.org>
2022-12-21 13:53:22 +0000

    zsh: document prezto modules, switch dir aliases to `dirHashes`

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -14,6 +14,8 @@ lib.mkIf config.sane.home-manager.enable
     dotDir = ".config/zsh";
     history.path = "/home/colin/.local/share/zsh/history";
 
+    # defaultKeymap = "vicmd"; # vim normal mode (cmd mode)
+
     # powerlevel10k prompt config
     initExtraBeforeCompInit = builtins.readFile ./p10k.zsh;
     initExtra = ''
@@ -35,19 +37,42 @@ lib.mkIf config.sane.home-manager.enable
     prezto = {
       enable = true;
       pmodules = [
+        # configures jobs to persist after shell exit; other basic niceties
         "environment"
+        # auto-titles terminal (e.g. based on cwd)
         "terminal"
+        # configures shortcuts like Ctrl+U=undo, Ctrl+L=clear
         "editor"
+        # adds `history-stat` alias, setopts for good history defaults
         "history"
+        # sets AUTO_CD, adds `d` alias to list directory stack, and `1`-`9` to cd that far back the stack
         "directory"
+        # helpers for term colors and styling. used by prompts? might be unnecessary
         "spectrum"
+        # configures aliases like `ll`, `la`, disables globbing for things like rsync
+        # adds aliases like `get` to fetch a file. also adds `http-serve` alias??
         "utility"
+        # tab completion. requires `utility` module prior to loading
+        # TODO: enable AUTO_PARAM_SLASH
         "completion"
         "prompt"
-        "git"  # TODO: delete? this defines git aliases i don't use, but might be used by prompt
+        # TODO: enable syntax-highlighting ?
       ];
       prompt.theme = "powerlevel10k";
       utility.safeOps = false;  # disable `mv` confirmation (and supposedly `rm`, too)
+      # editor.keymap = "vi";
+    };
+
+    dirHashes = {
+      # convenient `cd`-isms
+      "3rd" = "/home/colin/dev/3rd";
+      "dev" = "/home/colin/dev";
+      "knowledge" = "/home/colin/knowledge";
+      "nixos" = "/home/colin/nixos";
+      "nixpkgs" = "/home/colin/dev/3rd/nixpkgs";
+      "ref" = "/home/colin/ref";
+      "secrets" = "/home/colin/knowledge/secrets";
+      "uninsane" = "/home/colin/dev/uninsane";
     };
   };
 
@@ -56,12 +81,5 @@ lib.mkIf config.sane.home-manager.enable
     # common typos
     "cd.." = "cd ..";
     "cd../" = "cd ../";
-    # convenient `cd`-isms
-    "knowledge" = "cd ~/knowledge";
-    "nixos" = "cd ~/nixos";
-    "nixpkgs" = "cd ~/dev/3rd/nixpkgs";
-    "ref" = "cd ~/ref";
-    "secrets" = "cd ~/knowledge/secrets";
-    "uninsane" = "cd ~/dev/uninsane";
   };
 }
