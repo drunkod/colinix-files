875e923197169da33339083f698ba1616b04ef04
colin <colin@uninsane.org>
2023-01-02 11:34:02 +0000

    declare ~/private in fileSystems and reuse for pamMount

--- modules/impermanence/crypt.nix
+++ modules/impermanence/crypt.nix
@@ -29,6 +29,7 @@ let
       fi
     '';
   };
+  private-mount-unit = ''${utils.escapeSystemdPath "/home/colin/private"}.mount'';
 in lib.mkIf config.sane.impermanence.enable
 {
   systemd.services."prepareEncryptedClearedOnBoot" = rec {
@@ -69,12 +70,47 @@ in lib.mkIf config.sane.impermanence.enable
   sane.fs."${store.device}" = {
     # ensure the fs is mounted only after the mountpoint directory is created
     dir.reverseDepends = [ store.mount-unit ];
-    # HACK: this fs entry is provided by our mount service.
+    # HACK: this fs entry is provided by our mount unit.
     unit = store.mount-unit;
   };
   sane.fs."${store.underlying.path}" = {
     # don't mount until after the backing dir is setup correctly.
+    # TODO: this isn't necessary? the mount-unit already depends on prepareEncryptedClearOnBoot
+    # which depends on the underlying path?
     dir.reverseDepends = [ store.mount-unit ];
+  };
+
+  fileSystems."/home/colin/private" = {
+    device = "/nix/persist/home/colin/private";
+    fsType = "fuse.gocryptfs";
+    options = [
+      "noauto"  # don't try to mount, until the user logs in!
+      "allow_other"  # root ends up being the user that mounts this, so need to make it visible to `colin`.
+      "nodev"
+      "nosuid"
+      "quiet"
+      "defaults"
+    ];
+    noCheck = true;
+  };
+  sane.fs."/home/colin/private" = {
+    dir.reverseDepends = [
+      # mounting relies on the mountpoint first being created.
+      private-mount-unit
+      # ensure the directory is created during boot, and before user logs in.
+      "multi-user.target"
+    ];
+    # HACK: this fs entry is provided by the mount unit.
+    unit = private-mount-unit;
+  };
+  sane.fs."/nix/persist/home/colin/private" = {
+    dir.reverseDepends = [
+      # the mount unit relies on the source having first been created.
+      # (it also relies on the cryptfs having been seeded -- which we can't verify here).
+      private-mount-unit
+      # ensure the directory is created during boot, and before user logs in.
+      "multi-user.target"
+    ];
   };
 
   # TODO: could add this *specifically* to the .mount file for the encrypted fs?
