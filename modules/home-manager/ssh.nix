e46ab4ec14c5e5b147e270fb2e78642652149dfc
colin <colin@uninsane.org>
2023-01-06 15:05:01 +0000

    ssh: use sane.persist/sane.fs instead of home-manager to ensure keys

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -1,18 +1,21 @@
 { config, lib, pkgs, ... }:
 
-lib.mkIf config.sane.home-manager.enable
-{
-  home-manager.users.colin = let
-    host = config.networking.hostName;
-    user_pubkey = (import ../pubkeys.nix).users."${host}";
-    known_hosts_text = builtins.concatStringsSep
-      "\n"
-      (builtins.attrValues (import ../pubkeys.nix).hosts);
-  in { config, ...}: {
-    # ssh key is stored in private storage
-    home.file.".ssh/id_ed25519".source = config.lib.file.mkOutOfStoreSymlink "/home/colin/private/.ssh/id_ed25519";
-    home.file.".ssh/id_ed25519.pub".text = user_pubkey;
+let
+  host = config.networking.hostName;
+  user_pubkey = (import ../pubkeys.nix).users."${host}";
+  known_hosts_text = builtins.concatStringsSep
+    "\n"
+    (builtins.attrValues (import ../pubkeys.nix).hosts);
+  mkSymlink = target: {
+    symlink.target = target;
+    wantedBeforeBy = [ "multi-user.target" ];
+  };
+in lib.mkIf config.sane.home-manager.enable {
+  # ssh key is stored in private storage
+  sane.persist.home.private = [ ".ssh/id_ed25519" ];
+  sane.fs."/home/colin/.ssh/id_ed25519.pub" = mkSymlink (builtins.toString (pkgs.writeText "id_ed25519.pub" user_pubkey));
 
+  home-manager.users.colin = {
     programs.ssh.enable = true;
     # this optionally accepts multiple known_hosts paths, separated by space.
     programs.ssh.userKnownHostsFile = builtins.toString (pkgs.writeText "known_hosts" known_hosts_text);
