1839f87a4ef1e12d5d87ff19a03399e93e015813
colin <colin@uninsane.org>
2023-01-06 16:11:56 +0000

    vlc: handle the config file with sane.fs

--- modules/home-manager/vlc.nix
+++ modules/home-manager/vlc.nix
@@ -1,14 +1,14 @@
-{ config, lib, ... }:
+{ config, lib, sane-lib, ... }:
 
 lib.mkIf config.sane.home-manager.enable
 {
-  home-manager.users.colin.xdg.configFile."vlc/vlcrc".text =
+  sane.fs."/home/colin/.config/vlc/vlcrc" =
   let
     feeds = import ./feeds.nix { inherit lib; };
     podcastUrls = lib.strings.concatStringsSep "|" (
       builtins.map (feed: feed.url) feeds.podcasts
     );
-  in ''
+  in sane-lib.fs.wantedText ''
     [podcast]
     podcast-urls=${podcastUrls}
     [core]
