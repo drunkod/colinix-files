cc5cf9b6f4e2f3031cab7b46d5051234f2a41d14
colin <colin@uninsane.org>
2023-01-11 05:58:07 +0000

    flake: format

--- flake.nix
+++ flake.nix
@@ -37,106 +37,114 @@
     home-manager,
     sops-nix,
     uninsane
-  }: let
-    nixpkgsCompiledBy = local: nixpkgs.legacyPackages."${local}";
-    # return something which behaves like `pkgs`, for the provided system
-    # `local` = architecture of builder. `target` = architecture of the system beying deployed to
-    nixpkgsFor = local: target:
-      import ((nixpkgsCompiledBy local).path) {
-        crossSystem = target;
-        localSystem = local;
-      };
-    # evaluate ONLY our overlay, for the provided system
-    customPackagesFor = local: target:
-      let pkgs = nixpkgsFor local target;
-      in import ./pkgs/overlay.nix pkgs pkgs;
-    decl-host = { name, local, target }:
+  }:
     let
-      # XXX: we'd prefer to use `nixosSystem = (nixpkgsCompiledBy local).nixos`
-      # but it doesn't propagate config to the underlying pkgs, meaning it doesn't let you use
-      # non-free packages even after setting nixpkgs.allowUnfree.
-      nixosSystem = import ((nixpkgsCompiledBy local).path + "/nixos/lib/eval-config.nix");
-    in (nixosSystem {
-      # we use pkgs built for and *by* the target, i.e. emulation, by default.
-      # cross compilation only happens on explicit access to `pkgs.cross`
-      system = target;
-      modules = [
-        ./modules
-        (import ./hosts/instantiate.nix name)
-        home-manager.nixosModule
-        sops-nix.nixosModules.sops
-        {
-          nixpkgs.overlays = [
-            (import "${mobile-nixos}/overlay/overlay.nix")
-            uninsane.overlay
-            (import ./pkgs/overlay.nix)
-            (next: prev: rec {
-              # non-emulated packages build *from* local *for* target.
-              # for large packages like the linux kernel which are expensive to build under emulation,
-              # the config can explicitly pull such packages from `pkgs.cross` to do more efficient cross-compilation.
-              cross = (nixpkgsFor local target) // (customPackagesFor local target);
-              stable = nixpkgs-stable.legacyPackages."${target}";
+      nixpkgsCompiledBy = local: nixpkgs.legacyPackages."${local}";
+      # return something which behaves like `pkgs`, for the provided system
+      # `local` = architecture of builder. `target` = architecture of the system beying deployed to
+      nixpkgsFor = local: target:
+        import ((nixpkgsCompiledBy local).path) {
+          crossSystem = target;
+          localSystem = local;
+        };
+      # evaluate ONLY our overlay, for the provided system
+      customPackagesFor = local: target:
+        let pkgs = nixpkgsFor local target;
+        in import ./pkgs/overlay.nix pkgs pkgs;
 
-              # cross-compatible packages
-              # gocryptfs = cross.gocryptfs;
+      decl-host = { name, local, target }:
+        let
+          # XXX: we'd prefer to use `nixosSystem = (nixpkgsCompiledBy local).nixos`
+          # but it doesn't propagate config to the underlying pkgs, meaning it doesn't let you use
+          # non-free packages even after setting nixpkgs.allowUnfree.
+          nixosSystem = import ((nixpkgsCompiledBy local).path + "/nixos/lib/eval-config.nix");
+        in
+          (nixosSystem {
+            # we use pkgs built for and *by* the target, i.e. emulation, by default.
+            # cross compilation only happens on explicit access to `pkgs.cross`
+            system = target;
+            modules = [
+              ./modules
+              (import ./hosts/instantiate.nix name)
+              home-manager.nixosModule
+              sops-nix.nixosModules.sops
+              {
+                nixpkgs.overlays = [
+                  (import "${mobile-nixos}/overlay/overlay.nix")
+                  uninsane.overlay
+                  (import ./pkgs/overlay.nix)
+                  (next: prev: rec {
+                    # non-emulated packages build *from* local *for* target.
+                    # for large packages like the linux kernel which are expensive to build under emulation,
+                    # the config can explicitly pull such packages from `pkgs.cross` to do more efficient cross-compilation.
+                    cross = (nixpkgsFor local target) // (customPackagesFor local target);
+                    stable = nixpkgs-stable.legacyPackages."${target}";
 
-              # pinned packages:
-            })
-          ];
-        }
-      ];
-    });
+                    # cross-compatible packages
+                    # gocryptfs = cross.gocryptfs;
 
-    decl-bootable-host = { name, local, target }: rec {
-      nixosConfiguration = decl-host { inherit name local target; };
-      # this produces a EFI-bootable .img file (GPT with a /boot partition and a system (/ or /nix) partition).
-      # after building this:
-      #   - flash it to a bootable medium (SD card, flash drive, HDD)
-      #   - resize the root partition (use cfdisk)
-      #   - mount the part
-      #      - chown root:nixbld <part>/nix/store
-      #      - chown root:root -R <part>/nix/store/*
-      #      - chown root:root -R <part>/persist  # if using impermanence
-      #      - populate any important things (persist/, home/colin/.ssh, etc)
-      #   - boot
-      #   - if fs wasn't resized automatically, then `sudo btrfs filesystem resize max /`
-      #   - checkout this flake into /etc/nixos AND UPDATE THE FS UUIDS.
-      #   - `nixos-rebuild --flake './#<host>' switch`
-      img = nixosConfiguration.config.system.build.img;
-    };
-    hosts.servo = decl-bootable-host { name = "servo"; local = "x86_64-linux"; target = "x86_64-linux"; };
-    hosts.desko = decl-bootable-host { name = "desko"; local = "x86_64-linux"; target = "x86_64-linux"; };
-    hosts.lappy = decl-bootable-host { name = "lappy"; local = "x86_64-linux"; target = "x86_64-linux"; };
-    hosts.moby = decl-bootable-host { name = "moby"; local = "aarch64-linux"; target = "aarch64-linux"; };
-    # special cross-compiled variant, to speed up deploys from an x86 box to the arm target
-    # note that these *do* produce different store paths, because the closure for the tools used to cross compile
-    # v.s. emulate differ.
-    # so deploying foo-cross and then foo incurs some rebuilding.
-    hosts.moby-cross = decl-bootable-host { name = "moby"; local = "x86_64-linux"; target = "aarch64-linux"; };
-    hosts.rescue = decl-bootable-host { name = "rescue"; local = "x86_64-linux"; target = "x86_64-linux"; };
-  in {
-    # TODO: use catAttrs?
-    nixosConfigurations = builtins.mapAttrs (name: value: value.nixosConfiguration) hosts;
-    imgs = builtins.mapAttrs (name: value: value.img) hosts;
-    packages = let
-      allPkgsFor = sys: (customPackagesFor sys sys) // {
-        nixpkgs = nixpkgsCompiledBy sys;
-        uninsane = uninsane.packages."${sys}";
+                    # pinned packages:
+                  })
+                ];
+              }
+            ];
+          });
+
+      decl-bootable-host = { name, local, target }: rec {
+        nixosConfiguration = decl-host { inherit name local target; };
+        # this produces a EFI-bootable .img file (GPT with a /boot partition and a system (/ or /nix) partition).
+        # after building this:
+        #   - flash it to a bootable medium (SD card, flash drive, HDD)
+        #   - resize the root partition (use cfdisk)
+        #   - mount the part
+        #      - chown root:nixbld <part>/nix/store
+        #      - chown root:root -R <part>/nix/store/*
+        #      - chown root:root -R <part>/persist  # if using impermanence
+        #      - populate any important things (persist/, home/colin/.ssh, etc)
+        #   - boot
+        #   - if fs wasn't resized automatically, then `sudo btrfs filesystem resize max /`
+        #   - checkout this flake into /etc/nixos AND UPDATE THE FS UUIDS.
+        #   - `nixos-rebuild --flake './#<host>' switch`
+        img = nixosConfiguration.config.system.build.img;
+      };
+      hosts = {
+        servo = decl-bootable-host { name = "servo"; local = "x86_64-linux"; target = "x86_64-linux"; };
+        desko = decl-bootable-host { name = "desko"; local = "x86_64-linux"; target = "x86_64-linux"; };
+        lappy = decl-bootable-host { name = "lappy"; local = "x86_64-linux"; target = "x86_64-linux"; };
+        moby = decl-bootable-host { name = "moby"; local = "aarch64-linux"; target = "aarch64-linux"; };
+        # special cross-compiled variant, to speed up deploys from an x86 box to the arm target
+        # note that these *do* produce different store paths, because the closure for the tools used to cross compile
+        # v.s. emulate differ.
+        # so deploying foo-cross and then foo incurs some rebuilding.
+        moby-cross = decl-bootable-host { name = "moby"; local = "x86_64-linux"; target = "aarch64-linux"; };
+        rescue = decl-bootable-host { name = "rescue"; local = "x86_64-linux"; target = "x86_64-linux"; };
       };
     in {
-      x86_64-linux = allPkgsFor "x86_64-linux";
-      aarch64-linux = allPkgsFor "aarch64-linux";
-    };
-    templates = {
-      python-data = {
-        # initialize with:
-        # - `nix flake init -t '/home/colin/dev/nixos/#python-data'`
-        # then enter with:
-        # - `nix develop`
-        path = ./templates/python-data;
-        description = "python environment for data processing";
+      # TODO: use catAttrs?
+      nixosConfigurations = builtins.mapAttrs (name: value: value.nixosConfiguration) hosts;
+      imgs = builtins.mapAttrs (name: value: value.img) hosts;
+
+      packages =
+        let
+          allPkgsFor = sys: (customPackagesFor sys sys) // {
+            nixpkgs = nixpkgsCompiledBy sys;
+            uninsane = uninsane.packages."${sys}";
+          };
+        in {
+          x86_64-linux = allPkgsFor "x86_64-linux";
+          aarch64-linux = allPkgsFor "aarch64-linux";
+        };
+
+      templates = {
+        python-data = {
+          # initialize with:
+          # - `nix flake init -t '/home/colin/dev/nixos/#python-data'`
+          # then enter with:
+          # - `nix develop`
+          path = ./templates/python-data;
+          description = "python environment for data processing";
+        };
       };
     };
-  };
 }
 
