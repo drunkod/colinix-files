942e302afbd48cd1d6f20fc9dfb24fa8fecf5653
colin <colin@uninsane.org>
2023-01-11 07:51:19 +0000

    flake: fix crossFrom.aarch64-linux to actually be compiled from that system

--- flake.nix
+++ flake.nix
@@ -116,7 +116,7 @@
           # for large packages like the linux kernel which are expensive to build under emulation,
           # the config can explicitly pull such packages from `pkgs.cross` to do more efficient cross-compilation.
           crossFrom."x86_64-linux" = (prev.forceSystem "x86_64-linux" null).appendOverlays next.overlays;
-          crossFrom."aarch64-linux" = (prev.forceSystem "x86_64-linux" null).appendOverlays next.overlays;
+          crossFrom."aarch64-linux" = (prev.forceSystem "aarch64-linux" null).appendOverlays next.overlays;
         });
       };
 
