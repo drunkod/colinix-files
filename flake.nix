7b9910f287726b3430f7784e1243fcbac3a5469c
colin <colin@uninsane.org>
2023-01-11 07:37:01 +0000

    reorder `pkgs.cross` definition for better readability

--- flake.nix
+++ flake.nix
@@ -72,12 +72,14 @@
                 nixpkgs.overlays = [
                   (import "${mobile-nixos}/overlay/overlay.nix")
                   uninsane.overlay
-                ] ++ (builtins.attrValues self.overlays)
-                ++ [
                   (next: prev: {
+                    # for local != target we by default just emulate the target while building.
+                    # provide a `pkgs.cross.<pkg>` alias that consumers can use instead of `pkgs.<foo>`
+                    # to explicitly opt into non-emulated cross compilation for any specific package.
+                    # this is most beneficial for large packages with few pre-requisites -- like Linux.
                     cross = next.crossFrom."${local}";
                   })
-                ];
+                ] ++ (builtins.attrValues self.overlays);
               }
             ];
           });
