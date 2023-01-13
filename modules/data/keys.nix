6438971c8cb6536a54e4867fb5b597a85efb7de8
colin <colin@uninsane.org>
2023-01-13 05:56:51 +0000

    ssh keys: add github.com's pubkey

--- modules/data/keys.nix
+++ modules/data/keys.nix
@@ -21,4 +21,10 @@
       servo.root = root;
     };
   };
+
+  com.github = rec {
+    # documented here: <https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints>
+    # Github actually uses multiple keys -- one per format
+    root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+  };
 }
