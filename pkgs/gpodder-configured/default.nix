176a98879d5b08426e75e83504a32ba9a1ef5368
colin <colin@uninsane.org>
2023-01-14 05:25:16 +0000

    gpodder: auto-unsubscribe from extra feeds at start

--- pkgs/gpodder-configured/default.nix
+++ pkgs/gpodder-configured/default.nix
@@ -1,14 +1,17 @@
 { makeWrapper
 , gpodder
 , symlinkJoin
-, writeShellScript
+, writeScriptBin
 , config
 }:
 
+let
+  remove-extra = writeScriptBin "remove_extra.py" (builtins.readFile ./remove_extra.py);
+in
 # we use a symlinkJoin so that we can inherit the .desktop and icon files from the original gPodder
 (symlinkJoin {
   name = "gpodder-configured";
-  paths = [ gpodder ];
+  paths = [ gpodder remove-extra ];
   buildInputs = [ makeWrapper ];
 
   # gpodder keeps all its feeds in a sqlite3 database.
@@ -17,7 +20,8 @@
   # repeat imports are deduplicated by url, even when offline.
   postBuild = ''
     makeWrapper $out/bin/gpodder $out/bin/gpodder-configured \
-      --run "$out/bin/gpo import ~/.config/gpodderFeeds.opml"
+      --run "$out/bin/remove_extra.py ~/.config/gpodderFeeds.opml" \
+      --run "$out/bin/gpo import ~/.config/gpodderFeeds.opml" \
 
     # fix up the .desktop file to invoke our wrapped application
     orig_desktop=$(readlink $out/share/applications/gpodder.desktop)
