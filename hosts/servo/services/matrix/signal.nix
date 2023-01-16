72a2ab78f3488689758c0d4b30eb4ee54552a698
colin <colin@uninsane.org>
2023-01-16 11:54:32 +0000

    matrix: allow mautrix-signal to communicate with signald

--- hosts/servo/services/matrix/signal.nix
+++ hosts/servo/services/matrix/signal.nix
@@ -5,11 +5,18 @@
   services.mautrix-signal.environmentFile =
     config.sops.secrets.mautrix_signal_env.path;
 
+  services.mautrix-signal.settings.signal.socket_path = "/run/signald/signald.sock";
   services.mautrix-signal.settings.homeserver.domain = "uninsane.org";
   services.matrix-synapse.settings.app_service_config_files = [
     # auto-created by mautrix-signal service
     "/var/lib/mautrix-signal/signal-registration.yaml"
   ];
+
+  systemd.services.mautrix-signal.serviceConfig = {
+    # allow communication to signald
+    SupplementaryGroups = [ "signald" ];
+    ReadWritePaths = [ "/run/signald" ];
+  };
 
   sops.secrets.mautrix_signal_env = {
     sopsFile = ../../../../secrets/servo/mautrix_signal_env.bin;
