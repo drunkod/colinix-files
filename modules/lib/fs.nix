f6f8d0e19d2786430c7c17a086487e202806ef02
colin <colin@uninsane.org>
2023-01-27 01:51:35 +0000

    sane-lib.fs: new `wantedDir` helper

--- modules/lib/fs.nix
+++ modules/lib/fs.nix
@@ -2,6 +2,7 @@
 
 rec {
   wanted = lib.attrsets.unionOfDisjoint { wantedBeforeBy = [ "multi-user.target" ]; };
+  wantedDir = wanted { dir = {}; };
   wantedSymlink = symlink: wanted { inherit symlink; };
   wantedSymlinkTo = target: wantedSymlink { inherit target; };
   wantedText = text: wantedSymlink { inherit text; };
