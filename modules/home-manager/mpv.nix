840c2feba5756b9b9541c4c193b4e70814843bea
Colin <colin@uninsane.org>
2023-01-27 08:11:59 +0000

    mpv: specify config by sane.fs instead of home-manager

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -1,13 +1,11 @@
-{ config, lib, ... }:
+{ config, lib, sane-lib, ... }:
 
 lib.mkIf config.sane.home-manager.enable
 {
-  home-manager.users.colin.programs.mpv = {
-    enable = true;
-    config = {
-      save-position-on-quit = true;
-      keep-open = "yes";
-    };
-  };
+  # format is <key>=%<length>%<value>
+  sane.fs."/home/colin/.config/mpv/mpv.conf" = sane-lib.fs.wantedText ''
+    save-position-on-quit=%3%yes
+    keep-open=%3%yes
+  '';
 }
 
