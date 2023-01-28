392ad7c674a4279d1933efb1230eab1891e5b50b
Colin <colin@uninsane.org>
2023-01-28 03:39:13 +0000

    zsh: port from home-manager to nixos

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -1,5 +1,33 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, sane-lib, ... }:
 
+let
+  # powerlevel10k prompt config
+  # p10k.zsh is the auto-generated config, and i overwrite those defaults here, below.
+  p10k-overrides = ''
+    # powerlevel10k launches a gitstatusd daemon to accelerate git prompt queries.
+    # this keeps open file handles for any git repo i touch for 60 minutes (by default).
+    # that prevents unmounting whatever device the git repo is on -- particularly problematic for ~/private.
+    # i can disable gitstatusd and get slower fallback git queries:
+    # - either universally
+    # - or selectively by path
+    # see: <https://github.com/romkatv/powerlevel10k/issues/246>
+    typeset -g POWERLEVEL9K_VCS_DISABLED_DIR_PATTERN='(/home/colin/private/*|/home/colin/knowledge/*)'
+    # typeset -g POWERLEVEL9K_DISABLE_GITSTATUS=true
+
+    # show user@host also when logged into the current machine.
+    # default behavior is to show it only over ssh.
+    typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_CONTENT_EXPANSION='$P9K_CONTENT'
+  '';
+
+  prezto-init = ''
+    source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    source ${pkgs.zsh-prezto}/share/zsh-prezto/init.zsh
+  '';
+  vte-init = ''
+    source ${pkgs.vte}/etc/profile.d/vte.sh
+  '';
+in
 lib.mkIf config.sane.home-manager.enable
 {
   sane.persist.home.plaintext = [
@@ -11,98 +39,99 @@ lib.mkIf config.sane.home-manager.enable
     ".cache/gitstatus"
   ];
 
-  home-manager.users.colin.programs.zsh = {
+  programs.zsh = {
     enable = true;
-    enableSyntaxHighlighting = true;
-    enableVteIntegration = true;
-    history.ignorePatterns = [ "rm *" ];
-    dotDir = ".config/zsh";
-    history.path = "/home/colin/.local/share/zsh/history";
+    histFile = "$HOME/.local/share/zsh/history";
+    shellAliases = {
+      ":q" = "exit";
+      # common typos
+      "cd.." = "cd ..";
+      "cd../" = "cd ../";
+    };
+    setOptions = [
+      # defaults:
+      "HIST_IGNORE_DUPS"
+      "SHARE_HISTORY"
+      "HIST_FCNTL_LOCK"
+      # disable `rm *` confirmations
+      "rmstarsilent"
+    ];
 
-    # defaultKeymap = "vicmd"; # vim normal mode (cmd mode)
-
-    # powerlevel10k prompt config
-    # p10k.zsh is the auto-generated config, and i overwrite those defaults here, below.
-    initExtraBeforeCompInit = (builtins.readFile ./p10k.zsh) + ''
-      # powerlevel10k launches a gitstatusd daemon to accelerate git prompt queries.
-      # this keeps open file handles for any git repo i touch for 60 minutes (by default).
-      # that prevents unmounting whatever device the git repo is on -- particularly problematic for ~/private.
-      # i can disable gitstatusd and get slower fallback git queries:
-      # - either universally
-      # - or selectively by path
-      # see: <https://github.com/romkatv/powerlevel10k/issues/246>
-      typeset -g POWERLEVEL9K_VCS_DISABLED_DIR_PATTERN='(/home/colin/private/*|/home/colin/knowledge/*)'
-      # typeset -g POWERLEVEL9K_DISABLE_GITSTATUS=true
-
-      # show user@host also when logged into the current machine.
-      # default behavior is to show it only over ssh.
-      typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_CONTENT_EXPANSION='$P9K_CONTENT'
+    # .zshenv config:
+    shellInit = ''
+      ZDOTDIR=$HOME/.config/zsh
     '';
 
-    initExtra = ''
+    # .zshrc config:
+    interactiveShellInit =
+      (builtins.readFile ./p10k.zsh)
+    + p10k-overrides
+    + prezto-init
+    + vte-init
+    + ''
       # zmv is a way to do rich moves/renames, with pattern matching/substitution.
       # see for an example: <https://filipe.kiss.ink/zmv-zsh-rename/>
       autoload -Uz zmv
 
-      # disable `rm *` confirmations
-      setopt rmstarsilent
+      HISTORY_IGNORE='(sane-shutdown *|sane-reboot *|rm *)'
 
+      # extra aliases
+      # TODO: move to `shellAliases` config?
       function nd() {
         mkdir -p "$1";
         pushd "$1";
       }
+
+      # auto-cd into any of these dirs by typing them and pressing 'enter':
+      hash -d 3rd="/home/colin/dev/3rd"
+      hash -d dev="/home/colin/dev"
+      hash -d knowledge="/home/colin/knowledge"
+      hash -d nixos="/home/colin/nixos"
+      hash -d nixpkgs="/home/colin/dev/3rd/nixpkgs"
+      hash -d ref="/home/colin/ref"
+      hash -d secrets="/home/colin/knowledge/secrets"
+      hash -d tmp="/home/colin/tmp"
+      hash -d uninsane="/home/colin/dev/uninsane"
+      hash -d Videos="/home/colin/Videos"
     '';
 
-    # prezto = oh-my-zsh fork; controls prompt, auto-completion, etc.
-    # see: https://github.com/sorin-ionescu/prezto
-    prezto = {
-      enable = true;
-      pmodules = [
-        # configures jobs to persist after shell exit; other basic niceties
-        "environment"
-        # auto-titles terminal (e.g. based on cwd)
-        "terminal"
-        # configures shortcuts like Ctrl+U=undo, Ctrl+L=clear
-        "editor"
-        # adds `history-stat` alias, setopts for good history defaults
-        "history"
-        # sets AUTO_CD, adds `d` alias to list directory stack, and `1`-`9` to cd that far back the stack
-        "directory"
-        # helpers for term colors and styling. used by prompts? might be unnecessary
-        "spectrum"
-        # configures aliases like `ll`, `la`, disables globbing for things like rsync
-        # adds aliases like `get` to fetch a file. also adds `http-serve` alias??
-        "utility"
-        # tab completion. requires `utility` module prior to loading
-        # TODO: enable AUTO_PARAM_SLASH
-        "completion"
-        "prompt"
-        # TODO: enable syntax-highlighting ?
-      ];
-      prompt.theme = "powerlevel10k";
-      utility.safeOps = false;  # disable `mv` confirmation (and supposedly `rm`, too)
-      # editor.keymap = "vi";
-    };
-
-    dirHashes = {
-      # convenient `cd`-isms
-      "3rd" = "/home/colin/dev/3rd";
-      "dev" = "/home/colin/dev";
-      "knowledge" = "/home/colin/knowledge";
-      "nixos" = "/home/colin/nixos";
-      "nixpkgs" = "/home/colin/dev/3rd/nixpkgs";
-      "ref" = "/home/colin/ref";
-      "secrets" = "/home/colin/knowledge/secrets";
-      "tmp" = "/home/colin/tmp";
-      "uninsane" = "/home/colin/dev/uninsane";
-      "Videos" = "/home/colin/Videos";
-    };
+    syntaxHighlighting.enable = true;
   };
 
-  home-manager.users.colin.home.shellAliases = {
-    ":q" = "exit";
-    # common typos
-    "cd.." = "cd ..";
-    "cd../" = "cd ../";
-  };
+  # prezto = oh-my-zsh fork; controls prompt, auto-completion, etc.
+  # see: https://github.com/sorin-ionescu/prezto
+  # i believe this file is auto-sourced by the prezto init.zsh script.
+  sane.fs."/home/colin/.config/zsh/.zpreztorc" = sane-lib.fs.wantedText ''
+    zstyle ':prezto:*:*' color 'yes'
+
+    # modules (they ship with prezto):
+    # ENVIRONMENT: configures jobs to persist after shell exit; other basic niceties
+    # TERMINAL: auto-titles terminal (e.g. based on cwd)
+    # EDITOR: configures shortcuts like Ctrl+U=undo, Ctrl+L=clear
+    # HISTORY: `history-stat` alias, setopts for good history defaults
+    # DIRECTORY: sets AUTO_CD, adds `d` alias to list directory stack, and `1`-`9` to cd that far back the stack
+    # SPECTRUM: helpers for term colors and styling. used by prompts? might be unnecessary
+    # UTILITY: configures aliases like `ll`, `la`, disables globbing for things like rsync
+    #   adds aliases like `get` to fetch a file. also adds `http-serve` alias??
+    # COMPLETION: tab completion. requires `utility` module prior to loading
+    # TODO: enable AUTO_PARAM_SLASH
+    zstyle ':prezto:load' pmodule \
+      'environment' \
+      'terminal' \
+      'editor' \
+      'history' \
+      'directory' \
+      'spectrum' \
+      'utility' \
+      'completion' \
+      'prompt'
+
+    # default keymap. try also `vicmd` (vim normal mode, AKA "cmd mode") or `vi`.
+    zstyle ':prezto:module:editor' key-bindings 'emacs'
+
+    zstyle ':prezto:module:prompt' theme 'powerlevel10k'
+
+    # disable `mv` confirmation (and `rm`, too, unfortunately)
+    zstyle ':prezto:module:utility' safe-ops 'no'
+  '';
 }
