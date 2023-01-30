f66de76b7636e4ba915112a01520739d7d4cf2fe
Colin <colin@uninsane.org>
2023-01-30 01:54:57 +0000

    disable home-manager

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,9 +1,3 @@
-# docs:
-#   https://rycee.gitlab.io/home-manager/
-#   https://rycee.gitlab.io/home-manager/options.html
-#   man home-configuration.nix
-#
-
 { lib, config, pkgs, ... }:
 
 with lib;
@@ -15,28 +9,6 @@ in
     sane.home-manager.enable = mkOption {
       default = false;
       type = types.bool;
-    };
-  };
-
-  config = lib.mkIf cfg.enable {
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-
-    home-manager.users.colin = {
-
-      # run `home-manager-help` to access manpages
-      # or `man home-configuration.nix`
-      manual.html.enable = false;  # TODO: set to true later (build failure)
-      manual.manpages.enable = false;  # TODO: enable after https://github.com/nix-community/home-manager/issues/3344
-
-      home.stateVersion = "21.11";
-      home.username = "colin";
-      home.homeDirectory = "/home/colin";
-
-      programs = {
-        # XXX: unsure what this does?
-        home-manager.enable = true;
-      };
     };
   };
 }
