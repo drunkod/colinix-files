a55e8847ec1ba761952d975ea47fa807247b47dd
Colin <colin@uninsane.org>
2023-02-26 11:27:01 +0000

    cross compilation: disable tests for packages that flake

--- overlays/pins.nix
+++ overlays/pins.nix
@@ -64,6 +64,60 @@
     # - not ok 267 - tcp_bind_error_addrinuse_listen
     doCheck = false;
   });
+
+  llvmPackages_12 =
+    let
+      tools = prev.llvmPackages_12.tools.extend (self: super: {
+        libllvm = super.libllvm.overrideAttrs (upstream: {
+          # 2023/02/21: fix: "FAIL: LLVM-Unit :: ExecutionEngine/MCJIT/./MCJITTests/MCJITTest.return_global (2857 of 42084)"
+          # - nix log /nix/store/6vydavlxh1gvs0vmrkcx9qp67g3h7kcz-llvm-12.0.1.drv
+          # - wanted by sequoia, rav1e, rustc-1.66.1  (is this right?)
+          doCheck = false;
+          # upstream sets this with `rec`; TODO: have upstream refer to the final overrideAttrs version of the derivation instead of using rec.
+          cmakeFlags = next.lib.remove "-DLLVM_BUILD_TESTS=ON" upstream.cmakeFlags;
+        });
+      });
+    in
+      # see <nixpkgs:pkgs/development/compilers/llvm/12/default.nix>
+      # - we copy their strategy / attrset mutilation
+      prev.llvmPackages_12 // { inherit tools; } // tools;
+
+  llvmPackages_14 =
+    let
+      tools = prev.llvmPackages_14.tools.extend (self: super: {
+        libllvm = super.libllvm.overrideAttrs (upstream: {
+          # 2023/02/21: fix: "FAIL: LLVM-Unit :: ExecutionEngine/MCJIT/./MCJITTests/MCJITMultipleModuleTest.two_module_global_variables_case (43769 of 46988)"
+          # - nix log /nix/store/ib2yw6sajnhlmibxkrn7lj7chllbr85h-llvm-14.0.6.drv
+          # - wanted by clang-11-12-LLVMgold-path, compiler-rt-libc-12.0.1, clang-wrapper-12.0.1  (is this right?)
+          doCheck = false;
+          # upstream sets this with `rec`; TODO: have upstream refer to the final overrideAttrs version of the derivation instead of using rec.
+          cmakeFlags = next.lib.remove "-DLLVM_BUILD_TESTS=ON" upstream.cmakeFlags;
+        });
+      });
+    in
+      # see <nixpkgs:pkgs/development/compilers/llvm/14/default.nix>
+      # - we copy their strategy / attrset mutilation
+      prev.llvmPackages_14 // { inherit tools; } // tools;
+
+  llvmPackages_15 =
+    let
+      tools = prev.llvmPackages_15.tools.extend (self: super: {
+        libllvm = super.libllvm.override {
+          # 2023/02/21: fix: "FAIL: LLVM-Unit :: ExecutionEngine/MCJIT/./MCJITTests/..."
+          # llvm15 passes doCheck as a call arg, so we don't need to set cmakeFlags explicitly as in previous versions
+          doCheck = false;
+        };
+      });
+    in
+      prev.llvmPackages_15 // { inherit tools; } // tools;
+
+  modemmanager = prev.modemmanager.overrideAttrs (_upstream: {
+    # 2023/02/25
+    # "ERROR:test-modem-helpers.c:257:test_cmgl_response: assertion failed: (list != NULL)"
+    doCheck = false;
+    doInstallCheck = false;  # tests are run during install check??
+  });
+
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (py-next: py-prev: {
       ipython = py-prev.ipython.overridePythonAttrs (upstream: {
@@ -95,6 +149,23 @@
         #   "test_runtests_all"
         # ];
       });
+      twisted = py-prev.twisted.overridePythonAttrs (upstream: {
+        # 2023/02/25
+        # ```
+        # [ERROR]
+        # Traceback (most recent call last):
+        #   File "/nix/store/dcnsxrn8rsfk1dghah7md5glbbnfysq3-python3.10-twisted-22.10.0/lib/python3.10/site-packages/twisted/test/test_udp.py", line 645, in test_interface
+        #     self.assertEqual(self.client.transport.getOutgoingInterface(), "0.0.0.0")
+        #   File "/nix/store/dcnsxrn8rsfk1dghah7md5glbbnfysq3-python3.10-twisted-22.10.0/lib/python3.10/site-packages/twisted/internet/udp.py", line 449, in getOutgoingInterface
+        #     i = self.socket.getsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF)
+        # builtins.OSError: [Errno 92] Protocol not available
+        #
+        # twisted.test.test_udp.MulticastTests.test_interface
+        # ```
+        postPatch = upstream.postPatch + ''
+          echo 'MulticastTests.test_interface.skip = "Protocol not available"'>> src/twisted/test/test_udp.py
+        '';
+      });
     })
   ];
   strp = prev.srtp.overrideAttrs (_upstream: {
@@ -105,6 +176,16 @@
   tracker = prev.tracker.overrideAttrs (_upstream: {
     # 2023/02/22
     # "27/37 tracker:core / service                          TIMEOUT         60.37s   killed by signal 15 SIGTERM"
+    doCheck = false;
+  });
+  udisks2 = prev.udisks2.overrideAttrs (_upstream: {
+    # 2023/02/25
+    # "udisks-test:ERROR:test.c:61:on_completed_expect_failure: assertion failed (message == expected_message): ("Command-line `./udisks-test-helper 4' was signaled with signal SIGSEGV (11):\nstdout: `OK, deliberately causing a segfault\n'\nstderr: `qemu: uncaught target signal 11 (Segmentation fault) - core dumped\n'" == "Command-line `./udisks-test-helper 4' was signaled with signal SIGSEGV (11): OK, deliberately causing a segfault\n")"
+    doCheck = false;
+  });
+  upower = prev.upower.overrideAttrs (_upstream: {
+    # 2023/02/25
+    # "Tests.test_battery_state_guessing                          TIMEOUT        60.80s   killed by signal 15 SIGTERM"
     doCheck = false;
   });
 })
