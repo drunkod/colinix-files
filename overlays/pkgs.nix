ead9fd87d754b2426e4ebe487d5b137d7286d1ca
Colin <colin@uninsane.org>
2023-03-10 23:49:07 +0000

    overlays/pkgs.nix: cleanup: use `with next;` instead of bing so explicit

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -1,40 +1,41 @@
 (next: prev:
+  with next;
   let
     sane = rec {
       #### my own, non-upstreamable packages:
-      sane-scripts = next.callPackage ../pkgs/sane-scripts { };
-      feeds = next.callPackage ../pkgs/feeds { };
-      tow-boot-pinephone = next.callPackage ../pkgs/tow-boot-pinephone { };
-      tow-boot-rpi4 = next.callPackage ../pkgs/tow-boot-rpi4 { };
-      bootpart-uefi-x86_64 = next.callPackage ../pkgs/bootpart-uefi-x86_64 { };
-      bootpart-tow-boot-rpi-aarch64 = next.callPackage ../pkgs/bootpart-tow-boot-rpi-aarch64 { };
-      bootpart-u-boot-rpi-aarch64 = next.callPackage ../pkgs/bootpart-u-boot-rpi-aarch64 { };
-      rtl8723cs-firmware = next.callPackage ../pkgs/rtl8723cs-firmware { };
-      linux-megous = next.callPackage ../pkgs/linux-megous {
+      sane-scripts = callPackage ../pkgs/sane-scripts { };
+      feeds = callPackage ../pkgs/feeds { };
+      tow-boot-pinephone = callPackage ../pkgs/tow-boot-pinephone { };
+      tow-boot-rpi4 = callPackage ../pkgs/tow-boot-rpi4 { };
+      bootpart-uefi-x86_64 = callPackage ../pkgs/bootpart-uefi-x86_64 { };
+      bootpart-tow-boot-rpi-aarch64 = callPackage ../pkgs/bootpart-tow-boot-rpi-aarch64 { };
+      bootpart-u-boot-rpi-aarch64 = callPackage ../pkgs/bootpart-u-boot-rpi-aarch64 { };
+      rtl8723cs-firmware = callPackage ../pkgs/rtl8723cs-firmware { };
+      linux-megous = callPackage ../pkgs/linux-megous {
         kernelPatches = [
           prev.kernelPatches.bridge_stp_helper
           prev.kernelPatches.request_key_helper
         ];
       };
 
-      sublime-music-mobile = next.callPackage ../pkgs/sublime-music-mobile { };
+      sublime-music-mobile = callPackage ../pkgs/sublime-music-mobile { };
 
       #### customized packages
-      fluffychat-moby = next.callPackage ../pkgs/fluffychat-moby { };
-      gpodder-configured = next.callPackage ../pkgs/gpodder-configured { };
+      fluffychat-moby = callPackage ../pkgs/fluffychat-moby { };
+      gpodder-configured = callPackage ../pkgs/gpodder-configured { };
       # jackett doesn't allow customization of the bind address: this will probably always be here.
-      jackett = next.callPackage ../pkgs/jackett { inherit (prev) jackett; };
+      jackett = callPackage ../pkgs/jackett { inherit (prev) jackett; };
       # mozilla keeps nerfing itself and removing configuration options
-      firefox-unwrapped = next.callPackage ../pkgs/firefox-unwrapped { inherit (prev) firefox-unwrapped; };
+      firefox-unwrapped = callPackage ../pkgs/firefox-unwrapped { inherit (prev) firefox-unwrapped; };
 
       # patch rpi uboot with something that fixes USB HDD boot
-      ubootRaspberryPi4_64bit = next.callPackage ../pkgs/ubootRaspberryPi4_64bit { };
+      ubootRaspberryPi4_64bit = callPackage ../pkgs/ubootRaspberryPi4_64bit { };
 
-      gocryptfs = next.callPackage ../pkgs/gocryptfs { inherit (prev) gocryptfs; };
+      gocryptfs = callPackage ../pkgs/gocryptfs { inherit (prev) gocryptfs; };
 
-      browserpass = next.callPackage ../pkgs/browserpass { inherit (prev) browserpass; };
+      browserpass = callPackage ../pkgs/browserpass { inherit (prev) browserpass; };
 
-      fractal-latest = next.callPackage ../pkgs/fractal-latest { };
+      fractal-latest = callPackage ../pkgs/fractal-latest { };
 
       #### TEMPORARY: PACKAGES WAITING TO BE UPSTREAMED
 
@@ -44,14 +45,14 @@
         })
       ];
 
-      kaiteki = next.callPackage ../pkgs/kaiteki { };
-      lightdm-mobile-greeter = next.callPackage ../pkgs/lightdm-mobile-greeter { };
-      browserpass-extension = next.callPackage ../pkgs/browserpass-extension { };
-      gopass-native-messaging-host = next.callPackage ../pkgs/gopass-native-messaging-host { };
+      kaiteki = callPackage ../pkgs/kaiteki { };
+      lightdm-mobile-greeter = callPackage ../pkgs/lightdm-mobile-greeter { };
+      browserpass-extension = callPackage ../pkgs/browserpass-extension { };
+      gopass-native-messaging-host = callPackage ../pkgs/gopass-native-messaging-host { };
       tokodon = prev.libsForQt5.callPackage ../pkgs/tokodon { };
 
       # provided by nixpkgs patch or upstream preview
-      # splatmoji = next.callPackage ../pkgs/splatmoji { };
+      # splatmoji = callPackage ../pkgs/splatmoji { };
     };
   in sane // { inherit sane; }
 )
