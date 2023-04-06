87606214400c499c916dc5ad0525c5fe5c28a5a5
Colin <colin@uninsane.org>
2023-04-06 06:23:27 +0000

    enable nix's `auto-optimise-store`

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -41,6 +41,11 @@
     "nixpkgs=${pkgs.path}"
     "nixpkgs-overlays=${../..}/overlays"
   ];
+  # hardlinks identical files in the nix store to save 25-35% disk space.
+  # unclear _when_ this occurs. it's not a service.
+  # does the daemon continually scan the nix store?
+  # does the builder use some content-addressed db to efficiently dedupe?
+  nix.settings.auto-optimise-store = true;
 
   fonts = {
     enableDefaultFonts = true;
