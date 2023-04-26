32266158855a3bc1494b6593a5e73ca1f2915efc
Colin <colin@uninsane.org>
2023-04-26 00:31:24 +0000

    home: offlineimap: specify secrets using the sane.programs API

--- hosts/common/home/offlineimap.nix
+++ hosts/common/home/offlineimap.nix
@@ -4,14 +4,9 @@
 # - `offlineimap -a <accountname>`
 #
 # view account names inside the secrets file, listed below.
-{ config, sane-lib, ... }:
+{ ... }:
 
 {
-  sops.secrets."offlineimaprc" = {
-    owner = config.users.users.colin.name;
-    sopsFile = ../../../secrets/universal/offlineimaprc.bin;
-    format = "binary";
-  };
-  sane.programs.offlineimap.fs.".config/offlineimap/config" = sane-lib.fs.wantedSymlinkTo config.sops.secrets.offlineimaprc.path;
+  sane.programs.offlineimap.secrets.".config/offlineimap/config" = ../../../secrets/universal/offlineimaprc.bin;
 }
 
