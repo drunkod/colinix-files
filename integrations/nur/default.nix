9f21fbcedacdb909c7bceea6e6d83c20d39d6cb3
Colin <colin@uninsane.org>
2023-05-03 00:56:23 +0000

    nur: better docs for my entrypoint

--- integrations/nur/default.nix
+++ integrations/nur/default.nix
@@ -7,14 +7,20 @@
 #
 # rather, this is the entrypoint through which NUR finds my packages, modules, overlays.
 # it's reachable only from those using this repo via NUR.
+#
+# to manually query available packages, modules, etc, true:
+# - nix eval --impure --expr 'builtins.attrNames (import ./. {})'
 
-{ pkgs }:
+{ pkgs ? import <nixpkgs> {} }:
 let
   sanePkgs = import ../../pkgs/additional pkgs;
 in
 ({
+  # contains both packages not in nixpkgs, and patched versions of those in nixpkgs
   overlays.pkgs = import ../../overlays/pkgs.nix;
+  # contains only my packages which aren't in nixpkgs
   pkgs = sanePkgs;
+
   modules = import ../../modules { inherit (pkgs) lib; };
   lib = import ../../modules/lib { inherit (pkgs) lib; };
 } // sanePkgs)
