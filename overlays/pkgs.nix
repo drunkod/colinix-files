1b76f1d6430f4902fb5c5cca5e839e10827c7616
Colin <colin@uninsane.org>
2023-05-03 03:19:39 +0000

    cleanup: place packages into scopes
    this helps for my own packages which depend on eachother: they should now evaluate without an overlay

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -1,24 +1,18 @@
 (next: prev:
   let
-    additional = import ../pkgs/additional next;
+    additional = import ../pkgs/additional
+      { pkgs = next; lib = prev.lib; };
     python-packages = {
       pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
         (py-final: py-prev: import ../pkgs/python-packages { inherit (py-prev) callPackage; })
       ];
     };
 
-    # to avoid infinite recursion, the patched packages require *unpatched* inputs,
-    # but we don't want to just send `prev` naively, else patched packages might
-    # take dependencies on unpatched versions of other packages we patch; or they
-    # won't be able to use inputs from `additional`, etc.
-    #
-    # so, call the patched packages using the `next` package set, except with
-    # they packages we're "about to" patch replaced with their versions from `prev`.
-    #
-    # we could alternatively pass `pkgs = prev // { inherit (next) callPackage; }`,
-    patched = import ../pkgs/patched (next // patchedInputs);
-    patchedInputs = builtins.mapAttrs (name: _patched: prev."${name}") patched;
+    patched = import ../pkgs/patched
+      { pkgs = next; lib = prev.lib; unpatched = prev; };
 
     sane = additional // python-packages // patched;
-  in sane // { inherit sane; }
+  in sane // {
+    sane = next.recurseIntoAttrs sane;
+  }
 )
--- pkgs/patched/default.nix
+++ pkgs/patched/default.nix
@@ -1,18 +1,18 @@
-pkgs:
+{ pkgs, lib ? pkgs.lib, unpatched ? pkgs }:
 let
-  inherit (pkgs) callPackage;
-in {
-  # XXX: the `inherit`s here are because:
-  # - pkgs.callPackage draws from the _final_ package set.
-  # - pkgs.XYZ draws (selectively) from the _previous_ package set.
-  # see <overlays/pkgs.nix>
-  browserpass = callPackage ./browserpass { inherit (pkgs) browserpass; };
+  me = lib.makeScope pkgs.newScope (self: with self; {
+    # XXX: the `inherit`s here are because:
+    # - pkgs.callPackage draws from the _final_ package set.
+    # - unpatched.XYZ draws (selectively) from the _unpatched_ package set.
+    # see <overlays/pkgs.nix>
+    browserpass = callPackage ./browserpass { inherit (unpatched) browserpass; };
 
-  # mozilla keeps nerfing itself and removing configuration options
-  firefox-unwrapped = callPackage ./firefox-unwrapped { inherit (pkgs) firefox-unwrapped; };
+    # mozilla keeps nerfing itself and removing configuration options
+    firefox-unwrapped = callPackage ./firefox-unwrapped { inherit (unpatched) firefox-unwrapped; };
 
-  gocryptfs = callPackage ./gocryptfs { inherit (pkgs) gocryptfs; };
+    gocryptfs = callPackage ./gocryptfs { inherit (unpatched) gocryptfs; };
 
-  # jackett doesn't allow customization of the bind address: this will probably always be here.
-  jackett = callPackage ./jackett { inherit (pkgs) jackett; };
-}
+    # jackett doesn't allow customization of the bind address: this will probably always be here.
+    jackett = callPackage ./jackett { inherit (unpatched) jackett; };
+  });
+in me.packages me
