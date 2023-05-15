d024637edfd8049655a695c949e79fc5c4dd6c53
Colin <colin@uninsane.org>
2023-05-15 02:09:32 +0000

    "fix" gnome-control-center to run on sway again

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -55,6 +55,12 @@ let
     # mozilla keeps nerfing itself and removing configuration options
     firefox-unwrapped = callPackage ./patched/firefox-unwrapped { inherit (unpatched) firefox-unwrapped; };
 
+    gnome = unpatched.gnome.overrideScope' (gself: gsuper: {
+      gnome-control-center = gself.callPackage ./patched/gnome-control-center {
+        inherit (gsuper) gnome-control-center;
+      };
+    });
+
     gocryptfs = callPackage ./patched/gocryptfs { inherit (unpatched) gocryptfs; };
 
     # jackett doesn't allow customization of the bind address: this will probably always be here.
