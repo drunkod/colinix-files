b1240352813603ebecc580e0b0d6c41252b7cec4
Colin <colin@uninsane.org>
2023-05-15 11:03:05 +0000

    nixpatches: point kiwix-tools to upstream PR

--- nixpatches/list.nix
+++ nixpatches/list.nix
@@ -71,6 +71,7 @@ in [
   (fetchpatch' {
     # kiwix-tools: 3.4.0 -> 3.5.0
     saneCommit = "146f2449a19101ee202aa578a2b1d7377779890b";
+    prUrl = "https://github.com/NixOS/nixpkgs/pull/232020";
     hash = "sha256-Tqr8Ri8X2dDljDmWmjAQDRJGNenSFhrY/wr24h2JAh0=";
   })
 
