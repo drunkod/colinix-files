b1be78529b4bf2f9b91893755a73a7bf282ecbd4
Colin <colin@uninsane.org>
2023-06-14 07:39:18 +0000

    gpodder: apply update via upstream PR

--- nixpatches/list.nix
+++ nixpatches/list.nix
@@ -163,16 +163,22 @@ in [
   # related: <https://github.com/NixOS/nixpkgs/issues/236890#issuecomment-1585030861>
   ./2023-06-10-lemmy-downgrade.patch
 
-  (fetchpatch' {
-    title = "gpodder: wrap with missing `xdg-utils` path";
-    saneCommit = "10d0ac11bc083cbcf0d6340950079b3888095abf";
-    hash = "sha256-cu8L30ZiUJnWFGRR/SK917TC7TalzpGkurGkUAAxl54=";
-  })
+  # (fetchpatch' {
+  #   title = "gpodder: wrap with missing `xdg-utils` path";
+  #   saneCommit = "10d0ac11bc083cbcf0d6340950079b3888095abf";
+  #   hash = "sha256-cu8L30ZiUJnWFGRR/SK917TC7TalzpGkurGkUAAxl54=";
+  # })
+
+  # (fetchpatch' {
+  #   title = "gpodder: 3.10.21 -> 3.11.1";
+  #   saneCommit = "865f67ec1f1ed25a65ffb3afdf05fcb3979399e8";
+  #   hash = "sha256-QUR9eu5Ob4/iPMn2x0ACRsC7y0xVZ+vA7AXUjkF0p/s=";
+  # })
 
   (fetchpatch' {
     title = "gpodder: 3.10.21 -> 3.11.1";
-    saneCommit = "865f67ec1f1ed25a65ffb3afdf05fcb3979399e8";
-    hash = "sha256-QUR9eu5Ob4/iPMn2x0ACRsC7y0xVZ+vA7AXUjkF0p/s=";
+    prUrl = "https://github.com/NixOS/nixpkgs/pull/237526";
+    hash = "sha256-sSCAmmI5RI0WtcLqXIf8YgPw9Yw3tOD0kQJlE675ahs=";
   })
 
   (fetchpatch' {
