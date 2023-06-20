bb1a2c9dcbdb705b5b64fd51be324174118cae7a
Colin <colin@uninsane.org>
2023-06-20 00:33:10 +0000

    moby: remove ~/.config/sxmo/profile in favor of the nixos-level config options
    note that this reverts from a SXMO_SWAY_SCALE of 2.0 -> 1.5
    
    there may be other idiomatic ways to tune that

--- hosts/by-name/moby/polyfill.nix
+++ hosts/by-name/moby/polyfill.nix
@@ -5,19 +5,13 @@
       # touch screen
       SXMO_LISGD_INPUT_DEVICE = "/dev/input/by-path/platform-1c2ac00.i2c-event";
       # vol and power are detected correctly by upstream
+
+      # preferences
+      # N.B. some deviceprofiles explicitly set SXMO_SWAY_SCALE, overwriting what we put here.
+      SXMO_SWAY_SCALE = "1.5";
+      SXMO_ROTATION_GRAVITY = "12800";
+      DEFAULT_COUNTRY = "US";
+      BROWSWER = "librewolf";
     };
   };
-  # TODO: only populate this if sxmo is enabled?
-  sane.user.fs.".config/sxmo/profile" = sane-lib.fs.wantedText ''
-    # sourced by sxmo_init.sh
-    . sxmo_common.sh
-
-    export SXMO_SWAY_SCALE=1.5
-    export SXMO_ROTATION_GRAVITY=12800
-
-    export DEFAULT_COUNTRY=US
-    export BROWSER=librewolf
-
-    export SXMO_BG_IMG="$(xdg_data_path sxmo/background.jpg)"
-  '';
 }
