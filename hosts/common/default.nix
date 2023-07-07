c21efa005fe0aa8ceae5de90610d40e52f3f8246
Colin <colin@uninsane.org>
2023-07-07 23:34:12 +0000

    show package changes during nixos-rebuild switch

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -73,6 +73,15 @@
     };
   };
 
+  system.activationScripts.nixClosureDiff = {
+    supportsDryActivation = true;
+    text = ''
+      # show which packages changed versions or are new/removed in this upgrade
+      # source: <https://github.com/luishfonseca/dotfiles/blob/32c10e775d9ec7cc55e44592a060c1c9aadf113e/modules/upgrade-diff.nix>
+      ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
+    '';
+  };
+
   # XXX: twitter-color-emoji doesn't cross-compile; but not-fonts-emoji does
   # fonts = {
   #   enableDefaultFonts = true;
