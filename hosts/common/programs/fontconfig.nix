6e3e45a9f61a46a3009caa9e6089697453602337
Colin <colin@uninsane.org>
2023-07-08 02:27:21 +0000

    fontconfig: re-enable
    i'm not sure why this failed with lib.mkDefault

--- hosts/common/programs/fontconfig.nix
+++ hosts/common/programs/fontconfig.nix
@@ -2,13 +2,13 @@
 {
   fonts = lib.mkIf config.sane.programs.fontconfig.enabled {
     fontconfig.enable = true;
-    fontconfig.defaultFonts = lib.mkDefault {
+    fontconfig.defaultFonts = {
       emoji = [ "Font Awesome 6 Free" "Noto Color Emoji" ];
       monospace = [ "Hack" ];
       serif = [ "DejaVu Serif" ];
       sansSerif = [ "DejaVu Sans" ];
     };
-    enableDefaultFonts = lib.mkDefault true;
-    fonts = with pkgs; lib.mkDefault [ font-awesome noto-fonts-emoji hack-font ];
+    enableDefaultFonts = true;
+    fonts = with pkgs; [ font-awesome noto-fonts-emoji hack-font ];
   };
 }
