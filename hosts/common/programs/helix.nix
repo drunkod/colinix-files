a257d8d1a1d538366258670594763cee6cb42ef1
Colin <colin@uninsane.org>
2023-07-08 00:27:12 +0000

    helix: enable soft line wrapping

--- hosts/common/programs/helix.nix
+++ hosts/common/programs/helix.nix
@@ -8,5 +8,10 @@
     # - `hx --grammar fetch` and `hx --grammar build`
     # but otherwise, they ship as part of HELIX_RUNTIME, in the nix store
     # persist.plaintext = [ ".config/helix/runtime/grammars" ];
+    fs.".config/helix/config.toml".symlink.text = ''
+      # docs: <https://docs.helix-editor.com/configuration.html>
+      [editor.soft-wrap]
+      enable = true
+    '';
   };
 }
