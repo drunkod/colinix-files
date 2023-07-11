9502fb34c7ecf6b0da93d82ad922fe01526bcf0e
Colin <colin@uninsane.org>
2023-07-11 10:04:03 +0000

    tangram: don't need to create the dconf db: dconf will do that for us if need-be

--- hosts/common/programs/tangram.nix
+++ hosts/common/programs/tangram.nix
@@ -2,7 +2,7 @@
 # it views each tab as a distinct application, persisted, and where the 'home' button action is specific to each tab.
 # it supports ephemeral tabs, but UX is heavily geared to GCing those as early as possible.
 
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 let
   dconfProfile = pkgs.writeTextFile {
     name = "dconf-tangram-profile";
@@ -12,9 +12,6 @@ let
       system-db:site
     '';
   };
-  initTangramDconf = pkgs.writeShellScript "init-tangram-dconf" ''
-    test -f "$1" || ${lib.getBin pkgs.dconf}/bin/dconf compile "$1" "${pkgs.emptyDirectory}"
-  '';
 in
 {
   sane.programs.tangram = {
@@ -22,7 +19,6 @@ in
     # - `bwrap: Can't make symlink at /var/run: File exists`
     # see epiphany.nix for more info
     package = pkgs.tangram.overrideAttrs (upstream: {
-      # --set DCONF_PROFILE "${dconfProfile}/etc/dconf/profile/tangram"
       preFixup = ''
         gappsWrapperArgs+=(
           --set WEBKIT_DISABLE_SANDBOX_THIS_IS_DANGEROUS "1"
@@ -42,13 +38,5 @@ in
       # { type = "file"; path = ".config/dconf/tangram"; method = "bind"; }
       ".config/dconf"
     ];
-
-    fs."private/.config/dconf/tangram".generated = {
-      acl.mode = "0644";
-      command = [
-        "${initTangramDconf}"
-        "/home/colin/private/.config/dconf/tangram"
-      ];
-    };
   };
 }
