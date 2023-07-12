0d6a10bc3a2d371ef23fedc52a90ce33ee9ab209
Colin <colin@uninsane.org>
2023-07-12 08:58:35 +0000

    komikku: don't crash when crop_borders fails to find borders

--- pkgs/patched/komikku/default.nix
+++ pkgs/patched/komikku/default.nix
@@ -1,5 +1,6 @@
 { komikku
 , fetchFromGitLab
+, fetchpatch2
 }:
 komikku.overrideAttrs (upstream: {
   src = fetchFromGitLab {
@@ -8,4 +9,10 @@ komikku.overrideAttrs (upstream: {
     rev = "7dcf2b3d0ba685396872780b1ce75d01cbe02ebe";
     hash = "sha256-LzgHPuIpxy0ropiNycdxZP6onjK2JpMRqkkdmJGA4nE=";
   };
+  patches = [
+    (fetchpatch2 {
+      url = "https://git.uninsane.org/colin/mirror-komikku/commit/318fc0c975ba84ca4dcff405bc1bb8f5895bc5a6.diff";
+      hash = "sha256-mn81hCt5xrypJMOOiCOg8NthLjglXntTDsYpcdCg0E8=";
+    })
+  ];
 })
