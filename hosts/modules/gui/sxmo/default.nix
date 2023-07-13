5e078825681fb9f0109bcd8e01bc64916b38be79
Colin <colin@uninsane.org>
2023-07-13 06:58:48 +0000

    sxmo: back the superd log directory with a store

--- hosts/modules/gui/sxmo/default.nix
+++ hosts/modules/gui/sxmo/default.nix
@@ -135,6 +135,11 @@ in
           "sfeed"  # want this here so that the user's ~/.sfeed/sfeedrc gets created
           "superd"  # make superctl (used by sxmo) be on PATH
         ];
+
+        persist.cryptClearOnBoot = [
+          # builds to be 10's of MB per day
+          ".local/state/superd/logs"
+        ];
       };
     }
 
