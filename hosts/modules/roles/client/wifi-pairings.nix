9594c03d6649ce154c58cb6bde6ef59b01724817
Colin <colin@uninsane.org>
2023-07-15 06:38:50 +0000

    net: port iwd secrets to a .json file that i can make backend agnostic

--- hosts/modules/roles/client/wifi-pairings.nix
+++ hosts/modules/roles/client/wifi-pairings.nix
@@ -1,22 +1,21 @@
 { config, lib, pkgs, ... }:
 
 let
-  install-iwd = pkgs.static-nix-shell.mkBash {
-    pname = "install-iwd";
+  install-nm = pkgs.static-nix-shell.mkPython3Bin {
+    pname = "install-nm";
     src = ./.;
-    pkgs = [ "gnused" ];
   };
 in
 {
   config = lib.mkIf config.sane.roles.client {
-    sane.fs."/var/lib/iwd/.secrets.psk.stamp" = {
+    sane.fs."/var/lib/iwd/.install-nm.stamp" = {
       wantedBeforeBy = [ "iwd.service" ];
       generated.acl.mode = "0600";
       generated.command = [
-        "${install-iwd}/bin/install-iwd"
-        "/run/secrets/net"
+        "${install-nm}/bin/install-nm"
+        "/run/secrets/net/all.json"
         "/var/lib/iwd"
-        "/var/lib/iwd/.secrets.psk.stamp"
+        "--stamp" ".install-nm.stamp"
       ];
     };
   };
