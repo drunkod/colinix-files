10bdd3c05a5274ed41a8b945fcb7ac1b343b124b
Colin <colin@uninsane.org>
2023-07-18 20:21:48 +0000

    firefox: ship bypass-paywalls-clean extension

---
+++ pkgs/additional/firefox-extensions/bypass-paywalls-clean/default.nix
@@ -0,0 +1,31 @@
+{ lib
+, fetchFromGitLab
+, stdenv
+, zip
+}:
+
+stdenv.mkDerivation rec {
+  pname = "bypass-paywalls-clean";
+  version = "3.2.5.0";
+  src = fetchFromGitLab {
+    owner = "magnolia1234";
+    repo = "bypass-paywalls-firefox-clean";
+    rev = "v${version}";
+    hash = "sha256-FkAqzJisPdBiElX9ceQS3zfg8zwrsozOquHDagiRKiE=";
+  };
+
+  installPhase = ''
+    ${zip}/bin/zip -r $out ./*
+  '';
+
+  passthru = {
+    extid = "magnolia@12.34";
+  };
+
+  meta = {
+    homepage = "https://gitlab.com/magnolia1234/bypass-paywalls-firefox-clean";
+    description = "Add-on allows you to read articles from (supported) sites that implement a paywall.";
+    license = with lib.licenses; [ mit ];
+    maintainer = with lib.maintainers; [ colinsane ];
+  };
+}
