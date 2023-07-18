1709f64a69642ecd1532222e3d3b591ff40101b3
Colin <colin@uninsane.org>
2023-07-18 02:17:44 +0000

    zsh/starship: fix init

--- hosts/common/programs/zsh/starship.nix
+++ hosts/common/programs/zsh/starship.nix
@@ -1,13 +1,18 @@
+# starship prompt: <https://starship.rs/config/#prompt>
 { config, lib, pkgs, ...}:
 
 let
-  # starship prompt: <https://starship.rs/config/#prompt>
-  # populate ~/.config/starship.toml to customize
-  starship-init = ''
-    eval "$(${pkgs.starship}/bin/starship init zsh)"
-  '';
+  enabled = config.sane.zsh.starship;
+  toml = pkgs.formats.toml {};
 in {
   config = lib.mkIf config.sane.zsh.starship {
-    programs.zsh.interactiveShellInit = starship-init;
+    sane.programs.zsh = lib.mkIf enabled {
+      fs.".config/zsh/.zshrc".symlink.text = ''
+        eval "$(${pkgs.starship}/bin/starship init zsh)"
+      '';
+      fs.".config/starship.toml".symlink.target = toml.generate "starship.toml" {
+        format = "$all";
+      };
+    };
   };
 }
