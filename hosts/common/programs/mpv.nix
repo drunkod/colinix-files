558258c367f58a46243380ef64c98cb90cf80152
Colin <colin@uninsane.org>
2023-07-26 09:56:26 +0000

    moby: switch to mpv
    needs some more tuning, but ditching vlc means no more Qt, and it looks
    like i can tune the mpv UI to be substantially better for my wants

--- hosts/common/programs/mpv.nix
+++ hosts/common/programs/mpv.nix
@@ -1,3 +1,8 @@
+# mpv docs:
+# - <https://mpv.io/manual/master>
+# - <https://github.com/mpv-player/mpv/wiki>
+# curated mpv mods/scripts/users:
+# - <https://github.com/stax76/awesome-mpv>
 { ... }:
 
 {
@@ -8,6 +13,21 @@
       save-position-on-quit=%3%yes
       keep-open=%3%yes
     '';
+    fs.".config/mpv/script-opts/osc.conf".symlink.text = ''
+      # make the on-screen controls *always* visible
+      # unfortunately, this applies to full-screen as well
+      # - docs: <https://mpv.io/manual/master/#on-screen-controller-visibility>
+      visibility=always
+    '';
+
+    mime.priority = 200;  # default = 100; 200 means to yield to other apps
+    mime.associations."audio/flac" = "mpv.desktop";
+    mime.associations."audio/mpeg" = "mpv.desktop";
+    mime.associations."audio/x-vorbis+ogg" = "mpv.desktop";
+    mime.associations."video/mp4" = "mpv.desktop";
+    mime.associations."video/quicktime" = "mpv.desktop";
+    mime.associations."video/webm" = "mpv.desktop";
+    mime.associations."video/x-matroska" = "mpv.desktop";
   };
 }
 
