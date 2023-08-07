76d8921bdfdb8f9691d85ee14eca2c995083c841
Colin <colin@uninsane.org>
2023-08-07 08:29:06 +0000

    eg25-manager: fix by importing the legacy libgpiod v1 as it appeared nixpkgs 1 wk ago

--- pkgs/additional/eg25-manager/default.nix
+++ pkgs/additional/eg25-manager/default.nix
@@ -3,6 +3,7 @@
 
 { lib
 , stdenv
+, callPackage
 , fetchFromGitLab
 , gnugrep
 , meson
@@ -11,12 +12,19 @@
 , scdoc
 , curl
 , glib
-, libgpiod
 , libgudev
 , libusb1
 , modemmanager
 }:
 
+let
+  # eg25-manager needs to be made compatible with libgpiod 2.0 API. see:
+  # - <https://github.com/NixOS/mobile-nixos/pull/573#issuecomment-1666739462>
+  # - <https://gitlab.com/mobian1/eg25-manager/-/issues/45>
+  # nixpkgs libgpiod was bumped 2023-07-29:
+  # - <https://github.com/NixOS/nixpkgs/pull/246018>
+  libgpiod1 = callPackage ./libgpiod1.nix { };
+in
 stdenv.mkDerivation rec {
   pname = "eg25-manager";
   version = "0.4.6";
@@ -48,22 +56,20 @@ stdenv.mkDerivation rec {
   buildInputs = [
     curl
     glib
-    libgpiod
+    libgpiod1
     libgudev
     libusb1
     modemmanager
   ];
+
+  passthru = {
+    inherit libgpiod1;
+  };
 
   meta = with lib; {
     description = "Manager daemon for the Quectel EG25 mobile broadband modem";
     homepage = "https://gitlab.com/mobian1/eg25-manager";
     license = licenses.gpl3Plus;
     platforms = platforms.linux;
-    # needs to be made compatible with libgpiod 2.0 API. see:
-    # - <https://github.com/NixOS/mobile-nixos/pull/573#issuecomment-1666739462>
-    # - <https://gitlab.com/mobian1/eg25-manager/-/issues/45>
-    # nixpkgs libgpiod was bumped 2023-07-29:
-    # - <https://github.com/NixOS/nixpkgs/pull/246018>
-    broken = true;
   };
 }
