07408813db1f7232da6537cecd8aa9539e42d803
Colin <colin@uninsane.org>
2023-08-12 08:03:16 +0000

    python310Packages.keyring: re-enable cryptography dependency (it cross compiles now)

--- overlays/preferences.nix
+++ overlays/preferences.nix
@@ -10,22 +10,13 @@
 
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (pySelf: pySuper: {
-      # TODO(2023/08/02): cryptography (a dependency of komikku -> keyring -> secretstorage -> cryptography) doesn't cross compile
-      # so disable it. can be re-enabled in next staging -> master merge.
-      # see:
-      # - <https://github.com/NixOS/nixpkgs/pull/245287/files>
-      # - <https://github.com/NixOS/nixpkgs/pull/244135>
       keyring = (pySuper.keyring.override {
-        secretstorage = null;
-        jeepney = null;
-        # separate from `cryptography`: jaraco-classes doesn't cross compile
-        # but it looks like `keyring` has some _temporary_ fallback logic for
-        # when jaraco-classes isn't installed (i.e. may break in future).
+        # jaraco-classes doesn't cross compile, but it looks like `keyring`
+        # has some _temporary_ fallback logic for when jaraco-classes isn't
+        # installed (i.e. may break in future).
         jaraco-classes = null;
       }).overrideAttrs (upstream: {
         postPatch = (upstream.postPatch or "") + ''
-          sed -i /SecretStorage/d setup.cfg
-          sed -i /jeepney/d setup.cfg
           sed -i /jaraco.classes/d setup.cfg
         '';
       });
