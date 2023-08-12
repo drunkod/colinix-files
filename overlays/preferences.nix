436760a592f4382f423356dda89a613a32811328
Colin <colin@uninsane.org>
2023-08-12 07:58:15 +0000

    python310Packages.keyring: fix cross compilation

--- overlays/preferences.nix
+++ overlays/preferences.nix
@@ -18,10 +18,15 @@
       keyring = (pySuper.keyring.override {
         secretstorage = null;
         jeepney = null;
+        # separate from `cryptography`: jaraco-classes doesn't cross compile
+        # but it looks like `keyring` has some _temporary_ fallback logic for
+        # when jaraco-classes isn't installed (i.e. may break in future).
+        jaraco-classes = null;
       }).overrideAttrs (upstream: {
         postPatch = (upstream.postPatch or "") + ''
           sed -i /SecretStorage/d setup.cfg
           sed -i /jeepney/d setup.cfg
+          sed -i /jaraco.classes/d setup.cfg
         '';
       });
     })
