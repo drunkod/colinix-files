7d9d0ce8b5d64e0a31e70bc13b6914da0b2bd0d7
Colin <colin@uninsane.org>
2023-08-14 10:20:54 +0000

    g4music: wrapGAppsHook -> wrapGAppsHook4

--- pkgs/additional/g4music/default.nix
+++ pkgs/additional/g4music/default.nix
@@ -10,28 +10,34 @@
 , ninja
 , pkg-config
 , vala
-, wrapGAppsHook
+, wrapGAppsHook4
 }:
 stdenv.mkDerivation rec {
   pname = "g4music";
   version = "3.2";
+  # version = "3.1";
+  # version = "3.0";
+  # version = "2.4";
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
     owner = "neithern";
     repo = "g4music";
     rev = "v${version}";
-    hash = "sha256-BlHOYD4sOmJPNMzM5QA97Ah1N9tIat0Y6qxN6c5pmsw=";
+    hash = "sha256-BlHOYD4sOmJPNMzM5QA97Ah1N9tIat0Y6qxN6c5pmsw=";  # 3.2
+    # hash = "sha256-U3STRCsc3xIVVQZcC3nKNNmyjANUUcFPep8yN36N3NY=";  # 3.1
+    # hash = "sha256-Dg57BwEm6FNlXikQj1CfYovM5oIyhX2sM1G0FOdR6hU=";  # 3.0
+    # hash = "sha256-y3G34NQEKPc145jLr98I6GaflzzgMVCCFLdcJDVkn8I=";  # 2.4
   };
 
   nativeBuildInputs = [
     desktop-file-utils  # for update-desktop-database
+    gobject-introspection
     meson
     ninja
     pkg-config
     vala
-    wrapGAppsHook
-    gobject-introspection
+    wrapGAppsHook4
   ];
 
   buildInputs = [
