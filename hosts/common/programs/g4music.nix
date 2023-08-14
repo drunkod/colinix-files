f8b559bef1363b7fb0c79b036ddc647e335566d1
Colin <colin@uninsane.org>
2023-08-14 10:21:13 +0000

    g4music: docs on how to get it rendering correctly

--- hosts/common/programs/g4music.nix
+++ hosts/common/programs/g4music.nix
@@ -1,3 +1,10 @@
+# N.B.: requires first-run setup on moby:
+# - UI will render transparent
+# - click the hamburger (top-right: immediately left from close button)
+#   > Preferences
+#     > Background-blur mode: change from "Always" to "Never"
+#
+# the background blur is probably some dconf setting somewhere.
 { ... }:
 {
   sane.programs.g4music = {
