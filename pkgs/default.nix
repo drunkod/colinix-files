a1f79dc18ab8041ece806ab0e3e6ceec01576f6c
Colin <colin@uninsane.org>
2023-08-17 00:01:15 +0000

    komikku: use unpatched upstream now that my fix is in a release

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -112,8 +112,6 @@ let
     # jackett doesn't allow customization of the bind address: this will probably always be here.
     jackett = callPackage ./patched/jackett { inherit (unpatched) jackett; };
 
-    komikku = callPackage ./patched/komikku { inherit (unpatched) komikku; };
-
     phoc = callPackage ./patched/phoc { inherit (unpatched) phoc; };
 
 
