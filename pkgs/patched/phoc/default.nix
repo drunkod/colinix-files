cc6a0dd8b3d32d967e0a6778d5ac14da1be10671
Colin <colin@uninsane.org>
2023-08-25 01:32:45 +0000

    phoc: fix patching style to be better overridable

--- pkgs/patched/phoc/default.nix
+++ pkgs/patched/phoc/default.nix
@@ -1,11 +1,19 @@
-{ fetchpatch, phoc }:
-phoc.overrideAttrs (super: {
-  patches = super.patches or [] ++ [
-    (fetchpatch {
-      # this patch fixes some screen-blanking issues.
-      # not 100% necessary, but does give a better experience.
-      url = "https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/428.diff";
-      hash = "sha256-XaSpcjtAFbGpqSLOUvjFU84TRmjKhL0NPIDvEK4VUD4=";
-    })
-  ];
+{ lib
+, fetchpatch
+, phoc
+, ...
+}@attrs:
+(phoc.override (
+  (removeAttrs attrs [ "fetchpatch" "phoc" ])
+)).overrideAttrs (upstream: {
+  patches = lib.unique (
+    (upstream.patches or []) ++ [
+      (fetchpatch {
+        # this patch fixes some screen-blanking issues.
+        # not 100% necessary, but does give a better experience.
+        url = "https://gitlab.gnome.org/World/Phosh/phoc/-/merge_requests/428.diff";
+        hash = "sha256-XaSpcjtAFbGpqSLOUvjFU84TRmjKhL0NPIDvEK4VUD4=";
+      })
+    ]
+  );
 })
