ad9db9181201999e8c80fed10adc49ae9044bb88
Colin <colin@uninsane.org>
2023-08-28 08:03:27 +0000

    moby: ship eg25-control on user profile

--- hosts/by-name/moby/default.nix
+++ hosts/by-name/moby/default.nix
@@ -44,6 +44,7 @@
   sane.programs.tuiApps.enableFor.user.colin = false;  # visidata, others, don't compile well
   # disabled for faster deploys
   sane.programs.soundconverter.enableFor.user.colin = false;
+  sane.programs.eg25-control.enableFor.user.colin = true;
 
   # sane.programs.firefox.mime.priority = 300;  # prefer other browsers when possible
   # HACK/TODO: make `programs.P.env.VAR` behave according to `mime.priority`
