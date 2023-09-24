1ed1d8403d78b5e015da635613922a16c711c25b
Colin <colin@uninsane.org>
2023-09-24 21:38:20 +0000

    tuba: ship a friendly alias

--- hosts/common/programs/tuba.nix
+++ hosts/common/programs/tuba.nix
@@ -1,4 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  sane.programs.tuba.suggestedPrograms = [ "gnome-keyring" ];
+  sane.programs.tuba = {
+    package = pkgs.symlinkJoin {
+      # ship a `tuba` alias to the actual tuba binary, since i can never remember its name
+      name = "tuba";
+      paths = [
+        pkgs.tuba
+        (pkgs.runCommandLocal "tuba" {} ''
+          mkdir -p $out/bin
+          ln -s ${pkgs.tuba}/bin/dev.geopjr.Tuba $out/bin/tuba
+        '')
+      ];
+    };
+    suggestedPrograms = [ "gnome-keyring" ];
+  };
 }
