2600d6223c9c53465490394761646522ca007163
Colin <colin@uninsane.org>
2023-09-24 23:36:05 +0000

    tuba: fix FileDialog

--- hosts/common/programs/tuba.nix
+++ hosts/common/programs/tuba.nix
@@ -1,17 +1,20 @@
 { pkgs, ... }:
 {
   sane.programs.tuba = {
-    package = pkgs.symlinkJoin {
-      # ship a `tuba` alias to the actual tuba binary, since i can never remember its name
-      name = "tuba";
-      paths = [
-        pkgs.tuba
-        (pkgs.runCommandLocal "tuba" {} ''
-          mkdir -p $out/bin
-          ln -s ${pkgs.tuba}/bin/dev.geopjr.Tuba $out/bin/tuba
-        '')
-      ];
-    };
+    package = pkgs.tuba.overrideAttrs (upstream: {
+      postInstall = (upstream.postInstall or "") + ''
+        # ship a `tuba` alias to the actual tuba binary, since i can never remember its name
+        ln -s $out/bin/dev.geopjr.Tuba $out/bin/tuba
+      '';
+
+      preFixup = (upstream.preFixup or "") + ''
+        # 2023/09/24: fix so i can upload media when creating a post.
+        # see: <https://github.com/GeopJr/Tuba/issues/414#issuecomment-1732695845>
+        gappsWrapperArgs+=(
+          --prefix GDK_DEBUG , no-portals
+        )
+      '';
+    });
     suggestedPrograms = [ "gnome-keyring" ];
   };
 }
