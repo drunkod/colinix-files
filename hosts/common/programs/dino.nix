4a498ef1a9e8179eb15b7b3d1448645305345c82
Colin <colin@uninsane.org>
2023-10-07 08:27:35 +0000

    dino: docs: leave myself a TODO about niceness/priority

--- hosts/common/programs/dino.nix
+++ hosts/common/programs/dino.nix
@@ -16,13 +16,16 @@
 # outstanding bugs:
 # - mic is sometimes disabled at call start despite presenting as enabled
 #   - fix is to toggle it off -> on in the Dino UI
+# - default mic gain is WAY TOO MUCH (heavily distorted)
+# - TODO: dino should have more optimal niceness/priority to ensure it can process its buffers
+#
+# probably fixed:
 # - once per 1-2 minutes dino will temporarily drop mic input:
 #   - `rtp-WRNING: plugin.vala:148: Warning in pipeline: Can't record audio fast enough
 #   - this was *partially* fixed by bumping the pipewire mic buffer to 2048 samples (from ~512)
-#   - that fix can't extend to Dino itself except by patching its code (perhaps)
-#   - patching every `info.rate / 100` -> `info.rate / 50` in Dino to double the buffer size *seems* to have no effect
-#   - possible the 10ms buffer constant is inside `webrtc` itself
-#   - it's using gstreamer, so maybe other ways to introspect that/fix it
+#   - this was further fixed by setting PULSE_LATENCY_MSEC=20.
+#   - possibly Dino should be updated internally: `info.rate / 100` -> `info.rate / 50`.
+#     - i think that affects the batching for echo cancellation, adaptive gain control, etc.
 #
 { config, lib, ... }:
 let
