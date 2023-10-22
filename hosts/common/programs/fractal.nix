e7768572e59c8f8ab05d44f47afa843e2cdccda4
Colin <colin@uninsane.org>
2023-10-22 23:21:35 +0000

    fractal: update docs for fixing broken cache

--- hosts/common/programs/fractal.nix
+++ hosts/common/programs/fractal.nix
@@ -4,12 +4,19 @@
 # if it stalls while launching, especially with a brief message at bottom
 # "unable to open store"
 # then:
-# - remove ~/.local/share/stable
+# - remove ~/.local/share/stable/*
 #   - this might give I/O error, in which case remove the corresponding path under
 #     /nix/persist/home/colin/private (which can be found by correlating timestamps/sizes with that in ~/private/.local/share/stable).
 # - reboot (maybe necessary).
-# - TODO: unsure if necessary to delete the keyring entry and re-login, re-verify with other session, or not.
-#   - process above may leave you unable to send/receive encrypted messages.
+# - now you can send messages, and read messages in unencrypted rooms, but not read messages from encrypted rooms.
+# to fix encrypted message receipt:
+# - start from above (fractal closed, no ~/.local/share/stable/*)
+# - in ~/.local/share/keyrings/Default_keyring.keyring:
+#   - find the entry that says "display-name=Fractal: Matrix credentials for <mxid>"
+#   - remove that entry and all associated entries (i.e. ones with same number but different :attributeN)
+#   - REBOOT. otherwise keyring stuff seems to stay cached in RAM
+#   - login to Fractal. give an hour to sync.
+#   - it'll kick you back to a page asking you to cross-sign. open FluffyChat and do the emoji compare. success!
 { config, lib, pkgs, ... }:
 let
   cfg = config.sane.programs.fractal;
