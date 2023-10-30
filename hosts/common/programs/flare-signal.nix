745362e05e589b8e6bf7bded4903c2fc25822f05
Colin <colin@uninsane.org>
2023-10-30 11:02:51 +0000

    ship flare-signal on all GUI platforms

--- hosts/common/programs/flare-signal.nix
+++ hosts/common/programs/flare-signal.nix
@@ -1,6 +1,7 @@
-{ ... }:
+{ pkgs, ... }:
 {
   sane.programs.flare-signal = {
+    package = pkgs.flare-signal-nixified;
     persist.private = [
       # everything: conf, state, files, all opaque
       ".local/share/flare"
