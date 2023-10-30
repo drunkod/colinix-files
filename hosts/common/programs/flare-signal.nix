6d8f9edfd0e5f89b859747a922de028015bc7da3
Colin <colin@uninsane.org>
2023-10-30 14:02:24 +0000

    flare-signal: document problems

--- hosts/common/programs/flare-signal.nix
+++ hosts/common/programs/flare-signal.nix
@@ -1,3 +1,10 @@
+# Flare is a 3rd-party GTK4 Signal app.
+# UI is effectively a clone of Fractal.
+# compatibility:
+# - desko: works fine. pairs, and exchanges contact list (but not message history) with the paired device. exchanges future messages fine.
+# - moby (cross compiled): nope. it pairs, but can only *receive* messages and never *send* them.
+#   - even `rsync`ing the data and keyrings from desko -> moby, still fails in that same manner.
+#   - console shows error messages. quite possibly an endianness mismatch somewhere
 { pkgs, ... }:
 {
   sane.programs.flare-signal = {
@@ -5,6 +12,11 @@
     persist.private = [
       # everything: conf, state, files, all opaque
       ".local/share/flare"
+      # also persists a secret in ~/.local/share/keyrings. reset with:
+      # - `secret-tool search --all --unlock 'xdg:schema' 'de.schmidhuberj.Flare'`
+      # - `secret-tool clear 'xdg:schema' 'de.schmidhuberj.Flare'`
+      # and it persists some dconf settings (e.g. device name). reset with:
+      # - `dconf reset -f /de/schmidhuberj/Flare/`.
     ];
   };
 }
