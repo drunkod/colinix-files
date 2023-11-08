28d4a4b06513249287fa287d39b8b5ebf7bce2da
Colin <colin@uninsane.org>
2023-11-08 15:32:50 +0000

    persistence: move stores behind a byStore attr to support disabling persistence altogether (for e.g. rescue image)

--- hosts/common/programs/zsh/default.nix
+++ hosts/common/programs/zsh/default.nix
@@ -53,7 +53,7 @@ in
   config = mkMerge [
     ({
       sane.programs.zsh = {
-        persist.private = [
+        persist.byStore.private = [
           # we don't need to full zsh dir -- just the history file --
           # but zsh will sometimes backup the history file and symlinking just the file messes things up
           ".local/share/zsh"
