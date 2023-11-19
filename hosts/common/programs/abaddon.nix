256c85ba5cc5f9636ad216b47a6143778aaa2bdb
Colin <colin@uninsane.org>
2023-11-19 03:24:46 +0000

    abaddon: refer to upstream PR for platforms fix

--- hosts/common/programs/abaddon.nix
+++ hosts/common/programs/abaddon.nix
@@ -5,7 +5,7 @@
     # nixpkgs marks this explicitly as x86_64-only.
     # but i can build it for aarch64 here.
     # the only x86_64 runtime dependency is gnum4, via gtkmm (likely unused; build remnant).
-    # TODO: if it works reliably on aarch64, upstream this!
+    # see upstream nixpkgs PR: <https://github.com/NixOS/nixpkgs/pull/268433>
     package = pkgs.abaddon.overrideAttrs (upstream: {
       meta = upstream.meta // {
         platforms = lib.platforms.linux;
