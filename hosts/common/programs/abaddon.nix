61d5b9f048a859f3c9b8244d5e8c124f05dafe90
Colin <colin@uninsane.org>
2023-11-21 00:37:02 +0000

    abaddon: disable platform override (upstream has merged support for aarch64)

--- hosts/common/programs/abaddon.nix
+++ hosts/common/programs/abaddon.nix
@@ -2,16 +2,6 @@
 { lib, pkgs, ... }:
 {
   sane.programs.abaddon = {
-    # nixpkgs marks this explicitly as x86_64-only.
-    # but i can build it for aarch64 here.
-    # the only x86_64 runtime dependency is gnum4, via gtkmm (likely unused; build remnant).
-    # see upstream nixpkgs PR: <https://github.com/NixOS/nixpkgs/pull/268433>
-    package = pkgs.abaddon.overrideAttrs (upstream: {
-      meta = upstream.meta // {
-        platforms = lib.platforms.linux;
-      };
-    });
-
     fs.".config/abaddon/abaddon.ini".symlink.text = ''
       # see abaddon README.md for options.
       # at time of writing:
