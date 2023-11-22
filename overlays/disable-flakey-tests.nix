2dbde57f4660c3e520f1736d667ad3ce2f15b34a
Colin <colin@uninsane.org>
2023-11-22 01:42:33 +0000

    overlays/disable-flakey-tests: remove unneeded libwacom patch

--- overlays/disable-flakey-tests.nix
+++ overlays/disable-flakey-tests.nix
@@ -47,10 +47,10 @@ in {
   # 2023/07/28
   # "7/7 libwacom:all / pytest                               TIMEOUT        30.36s   killed by signal 15 SIGTERM"
   # N.B.: it passes on x86_64, but only if it's not CPU starved (i.e. nix build with -j1 if it fails)
-  libwacom = aarch64Only (_: {
-    doCheck = false;
-    mesonFlags = [ "-Dtests=disabled" ];
-  }) prev.libwacom;
+  # libwacom = aarch64Only (_: {
+  #   doCheck = false;
+  #   mesonFlags = [ "-Dtests=disabled" ];
+  # }) prev.libwacom;
 
   # 2023/02/22; 2023/10/30
   # "27/38 tracker:core / service                          TIMEOUT         60.37s   killed by signal 15 SIGTERM"
