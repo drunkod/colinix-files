10aea555ddd8d0ab97ad6fe7f14dfc34476980b3
Colin <colin@uninsane.org>
2023-11-25 04:46:24 +0000

    neovim: simplify implementation

--- hosts/common/programs/neovim.nix
+++ hosts/common/programs/neovim.nix
@@ -85,8 +85,61 @@ let
   plugin-config-lua = concatMapStrings (p: optionalString (p.type or "" == "lua") p.config) plugins;
 in
 {
-  # private because there could be sensitive things in the swap
   sane.programs.neovim = {
+    # package = config.programs.neovim.finalPackage;
+    package = pkgs.wrapNeovim pkgs.neovim-unwrapped {
+      withRuby = false;  #< doesn't cross-compile w/o binfmt
+      viAlias = true;
+      vimAlias = true;
+      configure = {
+        packages.plugins = {
+          start = plugin-packages;
+        };
+        customRC = ''
+          " let the terminal handle mouse events, that way i get OS-level ctrl+shift+c/etc
+          " this used to be default, until <https://github.com/neovim/neovim/pull/19290>
+          set mouse=
+
+          " copy/paste to system clipboard
+          set clipboard=unnamedplus
+
+          " screw tabs; always expand them into spaces
+          set expandtab
+
+          " at least don't open files with sections folded by default
+          set nofoldenable
+
+          " allow text substitutions for certain glyphs.
+          " higher number = more aggressive substitution (0, 1, 2, 3)
+          " i only make use of this for tex, but it's unclear how to
+          " apply that *just* to tex and retain the SyntaxRange stuff.
+          set conceallevel=2
+
+          " horizontal rule under the active line
+          " set cursorline
+
+          " highlight trailing space & related syntax errors (doesn't seem to work??)
+          " let c_space_errors=1
+          " let python_space_errors=1
+
+          " enable highlighting of leading/trailing spaces,
+          " and especially tabs
+          " source: https://www.reddit.com/r/neovim/comments/chlmfk/highlight_trailing_whitespaces_in_neovim/
+          set list
+          set listchars=tab:▷\·,trail:·,extends:◣,precedes:◢,nbsp:○
+
+          """"" PLUGIN CONFIG (vim)
+          ${plugin-config-viml}
+
+          """"" PLUGIN CONFIG (lua)
+          lua <<EOF
+          ${plugin-config-lua}
+          EOF
+        '';
+      };
+    };
+
+    # private because there could be sensitive things in the swap
     persist.byStore.private = [ ".cache/vim-swap" ];
     env.EDITOR = "vim";
     # git claims it should use EDITOR, but it doesn't!
@@ -95,58 +148,5 @@ in
     mime.associations."application/schema+json" = "nvim.desktop";
     mime.associations."plain/text" = "nvim.desktop";
     mime.associations."text/markdown" = "nvim.desktop";
-  };
-
-  programs.neovim = mkIf config.sane.programs.neovim.enabled {
-    # neovim: https://github.com/neovim/neovim
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    configure = {
-      packages.plugins = {
-        start = plugin-packages;
-      };
-      customRC = ''
-        " let the terminal handle mouse events, that way i get OS-level ctrl+shift+c/etc
-        " this used to be default, until <https://github.com/neovim/neovim/pull/19290>
-        set mouse=
-
-        " copy/paste to system clipboard
-        set clipboard=unnamedplus
-
-        " screw tabs; always expand them into spaces
-        set expandtab
-
-        " at least don't open files with sections folded by default
-        set nofoldenable
-
-        " allow text substitutions for certain glyphs.
-        " higher number = more aggressive substitution (0, 1, 2, 3)
-        " i only make use of this for tex, but it's unclear how to
-        " apply that *just* to tex and retain the SyntaxRange stuff.
-        set conceallevel=2
-
-        " horizontal rule under the active line
-        " set cursorline
-
-        " highlight trailing space & related syntax errors (doesn't seem to work??)
-        " let c_space_errors=1
-        " let python_space_errors=1
-
-        " enable highlighting of leading/trailing spaces,
-        " and especially tabs
-        " source: https://www.reddit.com/r/neovim/comments/chlmfk/highlight_trailing_whitespaces_in_neovim/
-        set list
-        set listchars=tab:▷\·,trail:·,extends:◣,precedes:◢,nbsp:○
-
-        """"" PLUGIN CONFIG (vim)
-        ${plugin-config-viml}
-
-        """"" PLUGIN CONFIG (lua)
-        lua <<EOF
-        ${plugin-config-lua}
-        EOF
-      '';
-    };
   };
 }
