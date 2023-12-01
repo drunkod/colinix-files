200b0dcf7cce7a65d544d1fc7969714122b24c34
Colin <colin@uninsane.org>
2023-12-01 03:28:32 +0000

    sxmo: better integrate the status components into waybar
    maybe i'll remove all the sxmo-specific logic eventually: it seems generalizable

--- hosts/modules/gui/sxmo/waybar-top.nix
+++ hosts/modules/gui/sxmo/waybar-top.nix
@@ -6,7 +6,15 @@
 
   modules-left = [ "sway/workspaces" ];
   modules-center = [ ];
-  modules-right = [ "custom/swaync" "custom/sxmo" ];
+  modules-right = [
+    "custom/swaync"
+    "clock"
+    "battery"
+    # "custom/sxmo/modem-state"
+    "custom/sxmo/modem-tech"
+    "custom/sxmo/modem-signal"
+    "custom/sxmo/wifi"
+  ];
 
   "sway/workspaces" = {
     all-outputs = true;
@@ -21,11 +29,47 @@
   };
 
   "custom/sxmo" = {
-    # this gives wifi state, batter, mic/speaker, lockstate, time all as one widget.
+    # this gives wifi state, battery, mic/speaker, lockstate, time all as one widget.
     # a good starting point, but may want to split these apart later to make things configurable.
-    # e.g. distinct vol-up & vol-down buttons next to the speaker?
+    # the values for this bar are computed in sxmo:configs/default_hooks/sxmo_hook_statusbar.sh
     exec = "sxmo_status.sh";
     interval = 1;
+    format = "{}";
+  };
+  # not ported: battery, ethernet
+  "custom/sxmo/modem-state" = {
+    exec = "cat /run/user/1000/sxmo_status/default/10-modem-state";
+    interval = 2;
+    format = "{}";
+  };
+  "custom/sxmo/modem-tech" = {
+    exec = "cat /run/user/1000/sxmo_status/default/11-modem-tech";
+    interval = 2;
+    format = "{}";
+  };
+  "custom/sxmo/modem-signal" = {
+    exec = "cat /run/user/1000/sxmo_status/default/12-modem-signal";
+    interval = 2;
+    format = "{}";
+  };
+  "custom/sxmo/wifi" = {
+    exec = "cat /run/user/1000/sxmo_status/default/30-wifi-status";
+    interval = 2;
+    format = "{}";
+  };
+  "custom/sxmo/volume" = {
+    exec = "cat /run/user/1000/sxmo_status/default/50-volume";
+    interval = 2;
+    format = "{}";
+  };
+  "custom/sxmo/state" = {
+    exec = "cat /run/user/1000/sxmo_status/default/90-state";
+    interval = 2;
+    format = "{}";
+  };
+  "custom/sxmo/time" = {
+    exec = "cat /run/user/1000/sxmo_status/default/99-time";
+    interval = 2;
     format = "{}";
   };
 }
