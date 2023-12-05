f350d7949cb737c4ff1909cdb72967dce67157ed
Colin <colin@uninsane.org>
2023-12-05 08:06:14 +0000

    sxmo: fix missing XDG_SESSION_TYPE env var

--- hosts/modules/gui/sway/default.nix
+++ hosts/modules/gui/sway/default.nix
@@ -277,6 +277,7 @@ in
         # - org.freedesktop.impl.portal.ScreenCast
         # - org.freedesktop.impl.portal.Screenshot
         enable = true;
+        package = cfg.package;
         extraPackages = [];  # nixos adds swaylock, swayidle, foot, dmenu by default
         # extraOptions = [ "--debug" ];
         # "wrapGAppsHook wrapper to execute sway with required environment variables for GTK applications."
@@ -287,7 +288,11 @@ in
         # this sets XDG_CURRENT_DESKTOP=sway
         # and makes sure that sway is launched dbus-run-session.
         wrapperFeatures.base = true;
-        package = cfg.package;
+        extraSessionCommands = ''
+          # XXX(2023/12/04): fixes a bug where sxmo somehow doesn't set XDG_SESSION_TYPE, preventing apps like Komikku from launching.
+          # wlroots should set this: idk where/why it's failing
+          export XDG_SESSION_TYPE=wayland
+        '';
       };
       programs.xwayland.enable = cfg.config.xwayland;
       # provide portals for:
