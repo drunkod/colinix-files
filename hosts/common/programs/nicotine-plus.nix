cf475c46961dd69b963e0104ffcdc451e6586472
Colin <colin@uninsane.org>
2024-01-21 03:56:33 +0000

    nicotine-plus: remove distro-specific symlink

--- hosts/common/programs/nicotine-plus.nix
+++ hosts/common/programs/nicotine-plus.nix
@@ -1,7 +1,14 @@
 # soulseek filesharing GUI app
-{ ... }:
+{ pkgs, ... }:
 {
   sane.programs.nicotine-plus = {
+    packageUnwrapped = pkgs.nicotine-plus.overrideAttrs (upstream: {
+      postInstall = (upstream.postInstall or "") + ''
+        # XXX: nixpkgs creates this symlink, seemingly just for convenience;
+        # third-party tools like `firejail` lack a profile for "nicotine-plus", and just for "nicotine" instead.
+        rm $out/bin/nicotine-plus
+      '';
+    });
     net = "vpn";
     # ".config/nicotine": contains the config file, with plaintext creds.
     # TODO: define this as a secret instead of persisting it.
